02b6e7ff1000f52f2b7fa16e9d61107c3cf8eb32
Alan Curiel <alannitro17@gmail.com>
2020-09-09 11:34:35 -0400

    Fix Bugs for validation

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,67 @@
-// Add some Javascript code here, to run on the front end.
+const submit = function (e) {
+    // prevent default form action from being carried out
+    e.preventDefault();
 
-console.log("Welcome to assignment 2!")
+    const input = document.querySelector("#firstname"),
+      input2 = document.querySelector("#lastname"),
+      input3 = document.querySelector("#birthyear"),
+      list = document.querySelector(".list"),
+      json = {
+        firstname: input.value,
+        lastname: input2.value,
+        birthyear: input3.value,
+        age: "",
+      },
+      body = JSON.stringify(json);
+
+    if (!input.value || !input2.value || !input3.value) {
+      console.warn("Empty field");
+      return;
+    }
+
+    if (
+      input3.value.length !== 4 ||
+      isNaN(+input3.value) ||
+      +input3.value > new Date().getFullYear()
+    ) {
+      console.warn("invalid year");
+      return;
+    }
+
+    fetch("/submit", {
+      method: "POST",
+      body,
+    })
+      .then((x) => x.json())
+      .then((x) => {
+        updatelist(list, x);
+      });
+
+    return false;
+  };
+
+  const updatelist = (list, items) => {
+    list.innerHTML = "";
+
+    items.map((i) => {
+      const text = document.createTextNode(
+          `First Name: ${i.firstname} | Last Name: ${i.lastname} | Birth Year: ${i.birthyear} | ~Age: ${i.age}`
+        ),
+        header = document.createElement("h2");
+
+      header.appendChild(text);
+      list.appendChild(header);
+    });
+  };
+
+  window.onload = function () {
+    const button = document.querySelector("button"),
+      list = document.querySelector(".list");
+    button.onclick = submit;
+
+    fetch("/data")
+      .then((x) => x.json())
+      .then((x) => {
+        updatelist(list, x);
+      });
+  };
