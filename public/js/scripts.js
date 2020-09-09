9c0717ab8aa68f348a4b897a56b25d27299d94fe
Jordan <climbjn189@gmail.com>
2020-09-08 23:13:22 -0400

    Wrote all the actual code, no styling

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,111 @@
-// Add some Javascript code here, to run on the front end.
+"use strict";
 
-console.log("Welcome to assignment 2!")
+//Function for marking an item off your list
+//If the item is not marked purchased then it will mark it purchased and then change its text
+//If the item is marked purchased then it will remove that mark
+function gotItem(e) {
+    //Get the table row that is the parent's parent
+    let tr = e.target.parentElement.parentElement;
+    //Swap purchased or not
+    if (tr.className === "purchased") {
+        tr.className = "";
+        e.target.innerHTML = "✓";
+    } else {
+        tr.className = "purchased";
+        e.target.innerHTML = "✗";
+    }
+}
+
+//Delete the purchased rows from both the local table and the server
+function deleteRows(e) {
+    //Find all the table rows marked purchased
+    let toDelete = document.getElementsByClassName("purchased");
+    let len = toDelete.length;
+    //Iterate through all of these elements
+    for (let i = 0; i < len; i++) {
+        //Get the index of that row - 2 as you must ignore the two header rows
+        let index = toDelete[0].rowIndex - 2;
+        //Put that info into json
+        let jsonDelete = {delete: 1, i: index};
+        const body = JSON.stringify(jsonDelete);
+
+        //Send it off as a POST request to the server and log the result
+        fetch("/submit", {
+            method:"POST",
+            body
+        })
+        .then(response => console.log(response.status))
+
+        //Actually remove the row
+        toDelete[0].remove();
+    }
+}
+
+//Function to get all the items from the server
+function getAll(e) {
+    console.log("Get all");
+}
+
+//Handles the click action
+const handleButton = function(e) {
+    //Ignores if you click on anything but a button
+    if (e.target.tagName === "BUTTON") {
+        //Route the button press to the correct function
+        if (e.target.id === "submit") {
+            submit(e);
+        } else if (e.target.className === "checkButton") {
+            gotItem(e);
+        } else if (e.target.id === "delete") {
+            deleteRows(e);
+        } else if (e.target.id === "sync") {
+            getAll(e);
+        }
+    }
+}
+
+//Submit the form data to the servers
+const submit = function( e ) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+
+    let json = {};
+    const input = document.forms["inputForm"].getElementsByTagName("input");
+    //Put the data into json format dynamically using the id as its key
+    for (let index = 0; index < input.length; index++) {
+        const element = input[index];
+        json[element["id"]] = element["value"]
+    }
+    const body = JSON.stringify(json)
+
+    //Fetch the data then conver the response into json and then insert it into the table
+    fetch( '/submit', {
+      method:'POST',
+      body 
+    })
+    .then(response => response.json())
+    .then( function( response ) {
+      //Find the table
+      const tableBody = document.getElementById("groceryBody");
+      //Insert a new row and then new cells
+      let row = tableBody.insertRow(-1);
+      let cell1 = row.insertCell(0);
+      let cell2 = row.insertCell(1);
+      let cell3 = row.insertCell(2);
+      let cell4 = row.insertCell(3);
+
+      //Fill those cells with the data gotten back from the server, this includes the new fields
+      cell1.innerHTML = response.quantity;
+      cell2.innerHTML = response.food;
+      //TODO: Change this to cal * quantity?
+      cell3.innerHTML = response.cal;
+      cell4.innerHTML = "<button class=\"checkButton\">✓</button>";
+    })
+
+    return false
+  }
+
+//When the page loads stick the click listener on there to handle all the buttons
+window.onload = function() {
+    document.addEventListener("click", handleButton, false);
+}
--- server.improved.js
+++ server.improved.js
@@ -1,17 +1,24 @@
+//Special thanks to the FoodData Central API from the U.S. Department of Agriculture
+//https://fdc.nal.usda.gov/api-guide.html
+//There is a chance since I am using the demo key that results are not gotten but hopefully the use of this won't exceed that
+
+const { stringify } = require('querystring')
+const { type } = require('os')
+
 const http = require( 'http' ),
       fs   = require( 'fs' ),
       // IMPORTANT: you must run `npm install` in the directory for this assignment
       // to install the mime library used in the following line of code
       mime = require( 'mime' ),
+      //REQUIRES ONE ADDITIONAL MODULE OF NODE-FETCH
+      fetch = require("node-fetch"),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+//Array for storing data
+let appdata = []
 
+//Base create server
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
     handleGet( request, response )    
@@ -20,6 +27,7 @@ const server = http.createServer( function( request,response ) {
   }
 })
 
+//Base handle get
 const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 ) 
 
@@ -30,6 +38,7 @@ const handleGet = function( request, response ) {
   }
 }
 
+//Extended handle post
 const handlePost = function( request, response ) {
   let dataString = ''
 
@@ -38,15 +47,56 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
 
-    // ... do something with the data here!!!
+    //parse the data into json
+    let data = JSON.parse( dataString );
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    //if the key delete is in the data then this is a deletion request, else it is an addition
+    if ("delete" in data) {
+      //delete the item at the index provided
+      appdata.splice(data["i"], 1);
+      //Send a quick response back that things are good
+      response.writeHead( 200, "OK", {'Content-Type': 'text/plain' });
+      response.end();
+      console.log(appdata);
+    } else {
+      //URL to be used to query the api
+      const qurl = "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&dataType=Branded&pageSize=1&query=";
+      
+      //Fetch the data from the api with the food added
+      fetch(qurl+data["food"])
+      .then(response => response.json())
+      .then(function(response) {
+        //Sort through the result JSON and look for the nutrients describing calories
+        //Also make sure we got an actual item back from the response
+        if (response.foods.length != 0) {
+          const nutrients = response.foods[0].foodNutrients;
+          for (let i = 0; i < nutrients.length; i++) {
+            const element = nutrients[i];
+            if (element.nutrientId === 1008) {
+              data["cal"] = element.value;
+            }
+          }
+        }
+        
+        //If the data was not in the API then we can't find it
+        if (!("cal" in data)) {
+          data["cal"] = "N/A";
+        }
+      })
+      .then(function() {
+        //Add the new data to the array and write back a response with the now complete object
+        appdata.push(data);
+        console.log(appdata);
+
+        response.writeHead( 200, "OK", {'Content-Type': 'application/json' });
+        response.end(JSON.stringify(data));
+      })
+    }
   })
 }
 
+//Base send file
 const sendFile = function( response, filename ) {
    const type = mime.getType( filename ) 
 
@@ -69,4 +119,5 @@ const sendFile = function( response, filename ) {
    })
 }
 
+//Hey! Listen!
 server.listen( process.env.PORT || port )
