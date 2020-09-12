33939c7ed17de36657e655ce6bb51f146215f67c
pooja-a-patel <62512120+pooja-a-patel@users.noreply.github.com>
2020-09-11 20:06:20 -0400

    Update server.improved.js

--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,161 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
-
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+  {
+    name_id: "Pooja",
+    orderDescription: "None",
+    flower: 0,
+    bouquet: 0,
+    price: 0
+  },
+  {
+    name_id: "Pooja",
+    orderDescription: "None",
+    flower: 2,
+    bouquet: 0,
+    price: 2
+  },
+  {
+    name_id: "Pooja",
+    orderDescription: "None",
+    flower: 0,
+    bouquet: 20,
+    price: 20
+  },
+  {
+    name_id: "Pooja",
+    orderDescription: "None",
+    flower: 2,
+    bouquet: 20,
+    price: 22
   }
-})
+];
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+const server = http.createServer(function(request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response); //communicate from HTML to server
   }
-}
+});
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+//use handleGet to display data structure (server) in UI (server to UI)
+const handleGet = function(request, response) {
+  const filename = dir + request.url.slice(1);
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else if (request.url === "/bookings") {
+    sendData(response, appdata);
+  } else {
+    sendFile(response, filename);
+  }
+};
 
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
+const handlePost = function(request, response) {
+  let dataString = "";
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  request.on("data", function(data) {
+    dataString += data;
+  });
 
-    // ... do something with the data here!!!
+  request.on("end", function() {
+    switch (request.url) {
+      case "/submit":
+        const booking = JSON.parse(dataString);
+        //const groomingPrice=
+        const price = addingServices(
+          parseInt(booking.flower),
+          parseInt(booking.bouquet)
+        );
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+        const newbooking = {
+          name_id: booking.name_id,
+          orderDescription: booking.orderDescription,
+          flower: parseInt(booking.flower),
+          bouquet: parseInt(booking.bouquet),
+          price: price
+        };
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+        appdata.push(newbooking);
 
-   fs.readFile( filename, function( err, content ) {
+        response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+        response.end();
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+        break;
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      case "/update":
+        const bookingToUpdate = JSON.parse(dataString);
 
-     }else{
+        const editedPrice = addingServices(
+          parseInt(bookingToUpdate.flower),
+          parseInt(bookingToUpdate.bouquet)
+        );
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+        const updatedbooking = {
+          parent_id: bookingToUpdate.parent_id,
+          name_id: bookingToUpdate.name_id,
+          orderDescription: bookingToUpdate.orderDescription,
+          flower: parseInt(bookingToUpdate.flower),
+          bouquet: parseInt(bookingToUpdate.bouquet),
+          price: editedPrice
+        };
 
-     }
-   })
-}
+        appdata.splice(bookingToUpdate.index, 1, updatedbooking);
 
-server.listen( process.env.PORT || port )
+        response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+        response.end();
+
+        break;
+
+      case "/delete":
+        const bookingToDelete = JSON.parse(dataString);
+        appdata.splice(bookingToDelete.bookingNumber, 1);
+        response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+        response.end();
+
+        break;
+
+      default:
+        response.end("404 Error: File not found");
+        break;
+    }
+  });
+};
+
+const sendData = function(response, bookings) {
+  const type = mime.getType(bookings);
+  response.writeHeader(200, { "Content-Type": type });
+  response.write(JSON.stringify({ data: bookings }));
+  response.end();
+};
+
+const addingServices = function(wantsFlower, bigDog) {
+  const baseGroomingPrice = 0;
+  const price = baseGroomingPrice + wantsFlower + bigDog;
+  return price;
+};
+
+const sendFile = function(response, filename) {
+  const type = mime.getType(filename);
+
+  fs.readFile(filename, function(err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
+
+server.listen(process.env.PORT || port);
