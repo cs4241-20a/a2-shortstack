9c6f102c86202e488f8b585e8f6c59ae229ed106
RubyLLL <xlyu00green@gmail.com>
2020-09-12 16:03:59 -0400

    go to glitch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,34 @@
 // Add some Javascript code here, to run on the front end.
 
 console.log("Welcome to assignment 2!")
+
+
+
+// class Book {
+//     constructor(title, author, isbn, stars, progress){
+//         this.title = title
+//         this.author = author
+//         this.isbn = isbn
+//         this.stars = stars
+//         this.progress = progress
+//     }
+// }
+
+// class UI {
+
+//     static addBookToList(book) {
+//        const list = document.querySelector('#book-list')
+//        const row = document.createElement('tr')
+
+//        row.innerHTML = `
+//         <td>${book.title}</td>
+//         <td>${book.author}</td>
+//         <td>${book.isbn}</td>
+//         <td>${book.stars}</td>
+//         <td>${book.progress}</td>
+//         <td><a href='#'><i class="far fa-times-circle delete" style="color: red;"></i></a></td>
+//         `
+
+//         list.appendChild(row)
+//     }
+// }
--- server.improved.js
+++ server.improved.js
@@ -7,9 +7,7 @@ const http = require( 'http' ),
       port = 3000
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -39,6 +37,7 @@ const handlePost = function( request, response ) {
 
   request.on( 'end', function() {
     console.log( JSON.parse( dataString ) )
+    console.log('hello')
 
     // ... do something with the data here!!!
 
