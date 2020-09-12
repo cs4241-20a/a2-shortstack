03977ce4d8960061404561e04d36467cff0c9560
Glitch (a2-zhechengsong) <none>
2020-09-12 15:45:13 +0000

    🕴📙 Updated with Glitch

--- server.improved.js
+++ server.improved.js
@@ -6,10 +6,7 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+const appdata = [  
 ]
 
 const server = http.createServer( function( request,response ) {
@@ -38,12 +35,67 @@ const handlePost = function( request, response ) {
   })
 
   request.on( 'end', function() {
+    
     console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
+    
+    // Calculating due time using start time and priority
+    var oneTask = JSON.parse ( dataString );
+    var endTime;
+    
+    // If time not entered assume start time at 12:00
+    if (oneTask.startTime === ""){
+      oneTask.startTime = "12:00";
+    }
+    
+    // Adding time based on priority
+    switch(parseInt(oneTask.prio)){
+      case 1: endTime = addTime(oneTask.startTime,30); break;
+      case 2: endTime = addTime(oneTask.startTime,60); break;
+      case 3: endTime = addTime(oneTask.startTime,120); break;
+      default: endTime = oneTask.startTime; break;
+    }
+    
+    // Add due time to the object
+    oneTask.endTime = endTime;
+    
+    // Function to transfer start time to int and add time
+    // return a string of edited time
+    function addTime(startTime, addMin){
+      var hour = parseInt(startTime.substring(0,2));
+      var min = parseInt(startTime.substring(3,5));
+      if(addMin ===30){
+        if(min > 30){
+          min-=30;
+          hour++;
+        }else{
+          min+=addMin;
+        }
+      }else if(addMin === 60){
+        hour++;
+      }else if(addMin === 120){
+        hour+=2;
+      }
+      if(hour>24){
+        hour = 0;
+      }
+      var minString, hourString;
+      if(min<10){
+        minString = "0" + min.toString();
+      }else{
+        minString = min.toString();
+      }
+      if(hour<10){
+        hourString = "0" + hour.toString();
+      }else{
+        hourString = hour.toString();
+      }
+      return hourString.toString() + ":" + minString.toString();
+    }
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
+    // Return the modified object back
+    response.end(JSON.stringify(oneTask))
+    //response.end(JSON.stringify(appdata))
   })
 }
 
