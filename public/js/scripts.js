1929aa94ae1a49f1728d1bd0931bf24deff1fa79
Adrianna Staszewska <41236648+adriannaStaszewska@users.noreply.github.com>
2020-09-14 16:30:07 -0400

    Added delete and visual representation of the habit grid

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,9 +5,9 @@ const submit = function( e ) {
     e.preventDefault()
 
     const habit = document.querySelector( '#task' ),
-        weeks = document.querySelector( '#weeks' )
-    json = { habit_name: habit.value, weeks:  weeks.value},
-        body = JSON.stringify( json )
+          weeks = document.querySelector( '#weeks' ),
+          json = { habit_name: habit.value, weeks:  weeks.value},
+          body = JSON.stringify( json )
 
     fetch( '/submit', {
         method:'POST',
@@ -17,14 +17,66 @@ const submit = function( e ) {
             // do something with the reponse
             return response.json()
         }).then(function ( json ) {
+        let habitView = document.getElementById("habitView")
         for(let i = 0; i<json.length; i++){
-            generateTable(JSON.parse(json[i]).weeks)
+            let div = document.createElement("div")
+            div.id = "habit_"+i
+            div.className = "habitBlock"
+            let spanE =  document.createElement("span")
+            spanE.className = "habitName"
+            let title = document.createTextNode(JSON.parse(json[i]).habit_name)
+            spanE.appendChild(title)
+            div.appendChild(spanE)
+            habitView.appendChild(div)
+            generateTable(JSON.parse(json[i]).weeks, div.id, JSON.parse(json[i]).days)
+            let delBtn = document.createElement("button")
+            div.appendChild(delBtn)
+            delBtn.innerText = "Delete"
+            delBtn.className = "delBtns"
+            delBtn.id = "delBtn_" + i
+            delBtn.onclick = deleteFunc
         }
     })
 
     return false
 }
 
+const editFunc = function (e) {
+    e.preventDefault();
+
+}
+
+const deleteFunc = function(e){
+    document.getElementById("habitView").innerHTML = "";
+    let json = {habit: e.target.getAttribute("id").split("_")[1]}
+    fetch( '/delete', {
+        method:'POST',
+        body: JSON.stringify(json)
+    })
+        .then( function( response ) {
+            return response.json()
+        }).then(function ( json ) {
+        let habitView = document.getElementById("habitView")
+        for(let i = 0; i<json.length; i++){
+            let div = document.createElement("div")
+            div.id = "habit_"+i
+            div.className = "habitBlock"
+            let spanE =  document.createElement("span")
+            spanE.className = "habitName"
+            let title = document.createTextNode(JSON.parse(json[i]).habit_name)
+            spanE.appendChild(title)
+            div.appendChild(spanE)
+            habitView.appendChild(div)
+            generateTable(JSON.parse(json[i]).weeks, div.id, JSON.parse(json[i]).days)
+            let delBtn = document.createElement("button")
+            div.appendChild(delBtn)
+            delBtn.innerText = "Delete"
+            delBtn.className = "delBtns"
+            delBtn.id = "delBtn_" + i
+            delBtn.onclick = deleteFunc
+        }
+    })
+}
 window.onload = function() {
     const submitBtn = document.querySelector( '#submitBtn' )
     submitBtn.onclick = submit
@@ -39,19 +91,20 @@ const processHabits = function(json){
     console.log(json)
 }
 
-const generateTable = function(rowNum){
-    let habitView = document.getElementById("habitView"),
+const generateTable = function(rowNum, id, days){
+    console.log(id)
+    let habitBlock = document.getElementById(id),
         table = document.createElement("table")
     for(let i = 0; i<rowNum; i++){
         let row = table.insertRow();
         for(let j = 0; j< 7; j++){
             let cell = row.insertCell();
-            let text = document.createTextNode("X")
+            let text = document.createTextNode(days[i][j])
             cell.appendChild(text)
         }
 
         table.appendChild(row);
     }
-    habitView.appendChild(table)
+    habitBlock.appendChild(table)
 
 }
--- server.improved.js
+++ server.improved.js
@@ -1,71 +1,88 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require('http'),
+    fs = require('fs'),
+    // IMPORTANT: you must run `npm install` in the directory for this assignment
+    // to install the mime library used in the following line of code
+    mime = require('mime'),
+    dir = 'public/',
+    port = 3000
 
 const appdata = [
-  {'habit_name': 'exercise', 'startDate':"08/14/2020", 'weeks':4, 'days':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 0, 0, 0], 'longest_streak':0}
+    {
+        'habit_name': 'exercise',
+        'startDate': "08/14/2020",
+        'weeks': 4,
+        'days': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        'longest_streak': 0
+    }
 ]
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
-  }
+const server = http.createServer(function (request, response) {
+    if (request.method === 'GET') {
+        handleGet(request, response)
+    } else if (request.method === 'POST') {
+        handlePost(request, response)
+    }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 )
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  } else{
-    sendFile( response, filename )
-  }
+const handleGet = function (request, response) {
+    const filename = dir + request.url.slice(1)
+    if (request.url === '/') {
+        sendFile(response, 'public/index.html')
+    } else {
+        sendFile(response, filename)
+    }
 }
 let dataStorage = []
 //dataStorage.push(appdata.toString())
-const handlePost = function( request, response ) {
-    request.on( 'data', function( data ) {
-      let newdata = JSON.parse(data)
-      let days = new Array(newdata.weeks*7).fill(0)
-      newdata.days = days
-    dataStorage.push(JSON.stringify(newdata))
-    console.log(dataStorage)
-  })
+const handlePost = function (request, response) {
+    console.log(request.url)
+    if (request.url === '/submit') {
+        request.on('data', function (data) {
+            let newdata = JSON.parse(data)
+            let days = []
+            for (let i = 0; i < newdata.weeks; i++) {
+                days[i] = new Array(7).fill("O")
+            }
+            newdata.days = days
+            dataStorage.push(JSON.stringify(newdata))
+            console.log(dataStorage)
+        })
+    } else if (request.url === '/delete') {
+      request.on('data', function (data) {
+          let toDelete = JSON.parse(data).habit
+          dataStorage.splice(toDelete, 1)
+      })
 
-  request.on( 'end', function() {
-
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end(JSON.stringify(dataStorage))
-  })
+    } else if (request.url === '/edit') {
+      request.on('data', function (data) {})
+     
+    }
+    request.on('end', function () {
+        response.writeHead(200, "OK", {'Content-Type': 'text/plain'})
+        response.end(JSON.stringify(dataStorage))
+    })
 }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+const sendFile = function (response, filename) {
+    const type = mime.getType(filename)
 
-   fs.readFile( filename, function( err, content ) {
+    fs.readFile(filename, function (err, content) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+        // if the error = null, then we've loaded the file successfully
+        if (err === null) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+            // status code: https://httpstatuses.com
+            response.writeHeader(200, {'Content-Type': type})
+            response.end(content)
 
-     }else{
+        } else {
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+            // file not found, error code 404
+            response.writeHeader(404)
+            response.end('404 Error: File Not Found')
 
-     }
-   })
+        }
+    })
 }
 
-server.listen(  process.env.PORT || port )
+server.listen(process.env.PORT || port)
