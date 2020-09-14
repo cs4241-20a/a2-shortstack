56d967f5df8e67d0061b670e01f335195a94fe01
Glitch (pradnyamahurkar-a2-shortstack-1) <none>
2020-09-14 15:23:38 +0000

    🙏🛳 Checkpoint ./server.improved.js:21845626/11

--- server.improved.js
+++ server.improved.js
@@ -60,7 +60,7 @@ const handlePost = function( request, response ) {
     // ... do something with the data here!!!
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end( dataString )
+    response.end( )
   })
 }
 
