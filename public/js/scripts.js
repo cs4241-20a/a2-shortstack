dbcd5caef661506d127aaa56540ed2dea50a3e06
athammana <aj@virpietech.com>
2020-09-14 01:43:35 -0400

    Almost working game, must fix underlines and score system

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,63 @@
-// Add some Javascript code here, to run on the front end.
+  const start = () => {
+  	newScrambled();
+  	let inp = document.querySelector('#answer');
+  	inp.addEventListener('keyup', (e) => {
+  		event.preventDefault();
+  		if (e.code === 'Enter') {
+  			guessWord(inp.value);
+  		}
+  	});
+  };
 
-console.log("Welcome to assignment 2!")
+  const changeInputUnderlines = () => {
+  	let length = document.querySelector('#scrambledWord').innerText.length,
+  		input = document.querySelector('#answer'),
+  		charWidth = 1,
+  		gap = 0.5 * charWidth,
+  		totalWidth = length * (charWidth + gap);
+  	input.maxLength = length;
+  	input.style.width = totalWidth;
+  	input.style.letterSpacing = `${gap}ch`;
+  	console.log(totalWidth, length, document.querySelector('#scrambledWord').innerHTML);
+  	input.style.background = `repeating-linear-gradient(to right, black 0, black ${charWidth}ch, transparent 0, transparent ${charWidth + gap}ch) 0 100% / ${totalWidth - gap}ch 2px no-repeat`;
+  	// input.style.background = `repeating-linear-gradient(90deg, dimgrey 0, dimgrey ${charWidth}ch, transparent 0, transparent ${charWidth}ch + ${gap}ch) 0 100%/ ${totalWidth}ch - ${gap}ch 2px no-repeat;`;
+  	// input.style.background = 
+  	console.log('l', input.style.background, $('#answer').css('color', 'green'));
+  }
+
+  const newScrambled = () => {
+  	fetch('/currentWord', {
+  		method: 'GET'
+  	}).then((res) => {
+  		res.text().then((newWord) => {
+  			document.querySelector('#scrambledWord').innerHTML = newWord;
+			changeInputUnderlines();
+  		});
+  	});
+  };
+
+  const guessWord = (guess) => {
+  	data = JSON.stringify({'guess': guess});
+  	fetch('/guess', {
+  		method: 'POST',
+  		body: data
+  	}).then((res) => {
+  		res.text().then((correct) => { // is either true or false to see if that was the correct word
+  			if (correct === 'false') {
+  				document.querySelector('#scrambledWord').classList.add('incorrect');
+  				console.log(document.querySelector('#scrambledWord').classList);
+  				setTimeout(() => document.querySelector('#scrambledWord').classList.remove('incorrect'), 2500)
+  			} else {
+				let score = document.querySelector('#currScore');
+				score.innerHTML = parseInt(score.innerHTML) + 1;
+				document.querySelector('#answer').value = '';
+				newScrambled();
+  			}
+  		});
+  	});
+  };
+
+
+  window.onload = function() {
+    start();
+  };
--- server.improved.js
+++ server.improved.js
@@ -1,72 +1,86 @@
 const http = require( 'http' ),
       fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
       mime = require( 'mime' ),
       dir  = 'public/',
-      port = 3000
+      port = 3000;
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+const randomwords = ['size','pipe','show','toy','zipper','throne','baby','seat','river','ocean','spade',
+'pump','cakes','skate','cat','vegetable','nut','furniture','tendency','car','sleet','truck','basket','writer',
+'fish','rock','ants','border','experience','kitty','flesh','servant','hydrant','planes','week','office',
+'dog','art','yak','distance','stocking','snails','playground','knot','curtain','wrench','daughter','seashore',
+'side','channel','cow','surprise','team','partner','paper','leg','arch','produce','bell','language','hope',
+'women','angle','cream','jar','respect','pigs','loaf','fly','time','uncle','move','earth','pies','flame',
+'door','place','wing','fact','cherries','need','knee','ground','key','farm','direction','crayon','authority',
+'idea','cake','winter','copper','son','cactus','caption','road','slope','trouble','finger','comparison'];
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+let scores = [],
+    currentWord = '';
+
+const server = http.createServer(function( request,response ) {
+  if (request.method === 'GET') {
+    handleGet(request, response);
+  } else if (request.method === 'POST'){
+    handlePost(request, response);
   }
 })
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+const handleGet = function(request, response) {
+  const filename = dir + request.url.slice(1);
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
+  if (request.url === '/') {
+    sendFile(response, 'public/index.html')
+  } else if (request.url === '/currentWord'){
+
+    currentWord = randomwords[Math.floor(Math.random() * randomwords.length)];
+
+    response.writeHead(200, {'Content-Type': 'text/plain' });
+    response.end(currentWord); 
+
+  } else {
+    sendFile(response, filename);
   }
 }
 
 const handlePost = function( request, response ) {
-  let dataString = ''
+  let dataString = '';
 
   request.on( 'data', function( data ) {
-      dataString += data 
+      dataString += data;
   })
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  if (request.url === '/guess'){
+    request.on( 'end', function() {
+      let guess = JSON.parse(dataString).guess;
 
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
+      response.writeHead( 200, "OK", {'Content-Type': 'text/plain' });
+      response.end((guess === currentWord).toString()); // sends the boolean in string format
+    })
+  } else {
+    response.writeHead(200);
+    response.end();
+  }
 }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+const sendFile = function(response, filename) {
+   const type = mime.getType(filename);
 
-   fs.readFile( filename, function( err, content ) {
+   fs.readFile(filename, function(err, content) {
 
      // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+     if (err === null) {
 
        // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+       response.writeHeader(200, {'Content-Type': type});
+       response.end(content);
 
-     }else{
+     } else {
 
        // file not found, error code 404
-       response.writeHeader( 404 )
+       response.writeHeader(404)
        response.end( '404 Error: File Not Found' )
 
      }
    })
 }
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port, () => console.log('Listening on Port:', port));
