f8f8e7822c01831741426b81514f1ff677c12592
Kirsten Hart <kirstenhart@Kirstens-MacBook-Pro.local>
2020-09-14 22:46:32 -0400

    minor changes to fix table

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,13 +2,14 @@ const addPlayer = function( e ) {
   // prevent default form action from being carried out
   e.preventDefault()
 
-  const number = document.querySelector( '#number' ),
-        firstName = document.querySelector( '#firstName' ),
-        lastName = document.querySelector( '#lastName' ),
-        goals = document.querySelector( '#goals' ),
+  const numberInput = document.querySelector( '#number' ),
+        firstNameInput = document.querySelector( '#firstName' ),
+        lastNameInput = document.querySelector( '#lastName' ),
+        goalsInput = document.querySelector( '#goals' ),
+        assistsInput = document.querySelector( '#assists' ),
+        
         table = document.querySelector( '#resultTable'),
-        assists = document.querySelector( '#assists' ),
-        json = { number: number.value, firstName: firstName.value, lastName: lastName.value, goals: goals.value, assists: assists.value },
+        json = { number: numberInput.value, firstName: firstNameInput.value, lastName: lastNameInput.value, goals: goalsInput.value, assists: assistsInput.value },
         body = JSON.stringify( json );
 
   fetch( '/addPlayer', {
@@ -26,7 +27,32 @@ const addPlayer = function( e ) {
   return false
 }
 
+const updateRoster = function(table, data){
+    var newRow = table.insertRow(-1);
+    var numberCell = newRow.insertCell(0);
+    var firstNameCell = newRow.insertCell(1);
+    var lastNameCell = newRow.insertCell(2);
+    var goalsCell = newRow.insertCell(3);
+    var assistsCell = newRow.insertCell(4);
+
+    numberCell.innerHTML = data.number;
+    firstNameCell.innerHTML = data.firstName;
+    lastNameCell.innerHTML = data.lastName;
+    goalsCell.innerHTML = data.goals;
+    assistsCell.innerHTML = data.assists;
+}
+
 window.onload = function() {
   const button = document.querySelector( 'button' )
   button.onclick = addPlayer
+
+  const table = document.querySelector( '#resultTable' )
+  fetch( '/appData' )
+  .then( function( response ){
+      return response.json()
+  })
+  .then( function( array ){
+      console.log( array );
+      array.forEach( element => updateRoster( table, JSON.parse( element ) ) )
+  })
 }
