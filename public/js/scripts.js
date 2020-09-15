96d2c32111c1fb73d46e386bfc908c6fcddb863c
athammana <aj@virpietech.com>
2020-09-15 00:23:38 -0400

    Readme and jslints

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,162 +1,163 @@
-  function start() { // runs a lot of the functions necessary for the first time (scrambled word, enter in the input)
-  	newScrambled();
-  	updateScores();
-  	updateCurrentScore();
-  	let inp = document.querySelector('#answer');
+'use strict';
 
-  	inp.addEventListener('keyup', (e) => {
-  		event.preventDefault();
+function start() { // runs a lot of the functions necessary for the first time (scrambled word, enter in the input)
+	newScrambled();
+	updateScores();
+	updateCurrentScore();
+	let inp = document.querySelector('#answer');
 
-  		if (e.code === 'Enter') {
-  			guessWord(inp.value);
-  		}
-  	});
-  };
+	inp.addEventListener('keyup', (e) => {
+		event.preventDefault();
+
+		if (e.code === 'Enter') {
+			guessWord(inp.value);
+		}
+	});
+}
 
 
-  function deleteEverything() {
-  	endGame();
-  	fetch('/deleteEverything', {
-  		method: 'GET'
-  	}).then((res) => {
+function deleteEverything() {
+	endGame();
+	fetch('/deleteEverything', {
+		method: 'GET'
+	}).then(() => {
 		document.querySelector('#currScore').innerHTML = 0;
 		updateScores();
 		newScrambled();
-  	});
-  }
+	});
+}
 
 
-  function hint() {
-  	let inp = document.querySelector('#answer');
+function hint() {
+	let inp = document.querySelector('#answer');
 
-  	fetch('/hint', {
+	fetch('/hint', {
   		method: 'POST',
   		body: inp.dataset.hint.length
-  	}).then((res) => {
+	}).then((res) => {
   		res.text().then(char => {
   			inp.dataset.hint = inp.dataset.hint ? inp.dataset.hint + char : char;
   			inp.value = inp.dataset.hint;
   		});
-  	});
-  }
+	});
+}
 
 
-  function updateCurrentScore() {
-  	fetch('/getCurrScore', {
-  		method: 'GET'
-  	}).then((res) => {
-  		res.text().then(score => {
-  			document.querySelector('#currScore').innerHTML = score;
-  		});
-  	})
-  };
+function updateCurrentScore() {
+	fetch('/getCurrScore', {
+		method: 'GET'
+	}).then((res) => {
+		res.text().then(score => {
+			document.querySelector('#currScore').innerHTML = score;
+		});
+	});
+}
 
 
-  function updateScores() {
+function updateScores() {
 	fetch('/getScores', {
-  		method: 'GET'
-  	}).then((res) => {
-  		res.text().then((scores) => {
-  			let insertDiv = document.querySelector('#serverscores');
+		method: 'GET'
+	}).then((res) => {
+		res.text().then((scores) => {
+			let insertDiv = document.querySelector('#serverscores');
 
-  			scores = JSON.parse(scores);
-  			scores = scores.sort((a, b) => parseInt(b.score) - parseInt(a.score));
-  			insertDiv.innerHTML = '';
+			scores = JSON.parse(scores);
+			scores = scores.sort((a, b) => parseInt(b.score) - parseInt(a.score));
+			insertDiv.innerHTML = '';
 
-  			scores.forEach((score, i) => {
-  				if (i < 5){
+			scores.forEach((score, i) => {
+				if (i < 5){
 	  				let ele = document.createElement('tr');
 
 	  				ele.innerHTML = `${score.name}: ${score.score}`;
 	  				insertDiv.appendChild(ele);
-  				}
-  			});
-  		});
-  	});
-  };
+				}
+			});
+		});
+	});
+}
 
 
-  function endGame() { // ends the game and sends score to the server
-  	let name = document.querySelector('#boardName').value ? document.querySelector('#boardName').value : '???';
-  	let date = new Date();
-  	let scorejson = JSON.stringify({"date": date, "name": name.toUpperCase(), "score": 0});
+function endGame() { // ends the game and sends score to the server
+	let name = document.querySelector('#boardName').value ? document.querySelector('#boardName').value : '???';
+	let date = new Date();
+	let scorejson = JSON.stringify({"date": date, "name": name.toUpperCase(), "score": 0});
 
-  	fetch('/newScore', {
-  		method: 'POST',
-  		body: scorejson
-  	}).then((res) => {
-  		updateScores();
-  		newScrambled();
-  		updateCurrentScore();
-  	});
-  };
+	fetch('/newScore', {
+		method: 'POST',
+		body: scorejson
+	}).then(() => {
+		updateScores();
+		newScrambled();
+		updateCurrentScore();
+	});
+}
 
 
-  function changeInputUnderlines() {  // Change all the css styling needed for the dynamic character underlines
-  	let length = document.querySelector('#scrambledWord').innerText.length,
-  		input = document.querySelector('#answer'),
-  		charWidth = 1,
-  		gap = 0.5 * charWidth,
-  		totalWidth = length * (charWidth + gap);
+function changeInputUnderlines() {  // Change all the css styling needed for the dynamic character underlines
+	let length = document.querySelector('#scrambledWord').innerText.length,
+		input = document.querySelector('#answer'),
+		charWidth = 1,
+		gap = 0.5 * charWidth,
+		totalWidth = length * (charWidth + gap);
 
 	input.value = '';
 	input.dataset.hint = '';
-  	input.maxLength = length;
-  	input.style.width = `${totalWidth}ch`;
-  	input.style.letterSpacing = `${gap}ch`;
-  	input.style.background = `repeating-linear-gradient(to right, black 0, black ${charWidth}ch, transparent 0, transparent ${charWidth + gap}ch) 0 100% / ${totalWidth - gap}ch 2px no-repeat`;
-  };
+	input.maxLength = length;
+	input.style.width = `${totalWidth}ch`;
+	input.style.letterSpacing = `${gap}ch`;
+	input.style.background = `repeating-linear-gradient(to right, black 0, black ${charWidth}ch, transparent 0, transparent ${charWidth + gap}ch) 0 100% / ${totalWidth - gap}ch 2px no-repeat`;
+}
 
 
-  function newScrambled() { // Changes the scrambled word to a new one from the server
-  	document.querySelector('#scrambledWord').innerHTML = 'Loading...';
-  	fetch('/currentWord', {
-  		method: 'GET'
-  	}).then((res) => {
-  		res.text().then((newWord) => {
-  			document.querySelector('#scrambledWord').innerHTML = newWord;
-			changeInputUnderlines();
-  		});
-  	});
-  };
+function newScrambled() { // Changes the scrambled word to a new one from the server
+	document.querySelector('#scrambledWord').innerHTML = 'Loading...';
+	fetch('/currentWord', {
+		method: 'GET'
+	}).then((res) => {
+		res.text().then((newWord) => {
+			document.querySelector('#scrambledWord').innerHTML = newWord;
+		changeInputUnderlines();
+		});
+	});
+}
 
 
-  function guessWord(guess) { // takes in a value and sends it to the server to see if its correct
-  	data = JSON.stringify({'guess': guess});
-  	fetch('/guess', {
-  		method: 'POST',
-  		body: data
-  	}).then((res) => {
-  		res.text().then((correct) => { // is either true or false to see if that was the correct word
-  			let scrambled = document.querySelector('#scrambledWord')
-  			let inp = document.querySelector('#answer');
+function guessWord(guess) { // takes in a value and sends it to the server to see if its correct
+	let data = JSON.stringify({'guess': guess});
+	fetch('/guess', {
+		method: 'POST',
+		body: data
+	}).then((res) => {
+		res.text().then((correct) => { // is either true or false to see if that was the correct word
+			let scrambled = document.querySelector('#scrambledWord');
 
-  			if (correct === 'false') {
+			if (correct === 'false') {
 
-  				scrambled.classList.add('incorrect');
-  				setTimeout(() => scrambled.classList.remove('incorrect'), 2500);
-  			} else {
-				scrambled.classList.remove('incorrect')
+				scrambled.classList.add('incorrect');
+				setTimeout(() => scrambled.classList.remove('incorrect'), 2500);
+			} else {
+				scrambled.classList.remove('incorrect');
 				updateCurrentScore();
 				newScrambled();
-  			}
-  		});
-  	});
-  };
+			}
+		});
+	});
+}
 
 
- function answerkey() { // prints the answer key
- 	let randomwords = ['size','pipe','show','toy','zipper','throne','baby','seat','river','ocean','spade',
-	'pump','cakes','skate','cat','vegetable','nut','furniture','tendency','car','sleet','truck','basket','writer',
-	'fish','rock','ants','border','experience','kitty','flesh','servant','hydrant','planes','week','office',
-	'dog','art','yak','distance','stocking','snails','playground','knot','curtain','wrench','daughter','seashore',
-	'side','channel','cow','surprise','team','partner','paper','leg','arch','produce','bell','language','hope',
-	'women','angle','cream','jar','respect','pigs','loaf','fly','time','uncle','move','earth','pies','flame',
-	'door','place','wing','fact','cherries','need','knee','ground','key','farm','direction','crayon','authority',
-	'idea','cake','winter','copper','son','cactus','caption','road','slope','trouble','finger','comparison'];
- 	console.log(randomwords);
- }
+function answerkey() { // prints the answer key
+	let randomwords = ['size', 'pipe', 'show', 'toy', 'zipper', 'throne', 'baby', 'seat', 'river', 'ocean', 'spade',
+		'pump', 'cakes', 'skate', 'cat', 'vegetable', 'nut', 'furniture', 'tendency', 'car', 'sleet', 'truck', 'basket', 'writer',
+		'fish', 'rock', 'ants', 'border', 'experience', 'kitty', 'flesh', 'servant', 'hydrant', 'planes', 'week', 'office',
+		'dog', 'art', 'yak', 'distance', 'stocking', 'snails', 'playground', 'knot', 'curtain', 'wrench', 'daughter', 'seashore',
+		'side', 'channel', 'cow', 'surprise', 'team', 'partner', 'paper', 'leg', 'arch', 'produce', 'bell', 'language', 'hope',
+		'women', 'angle', 'cream', 'jar', 'respect', 'pigs', 'loaf', 'fly', 'time', 'uncle', 'move', 'earth', 'pies', 'flame',
+		'door', 'place', 'wing', 'fact', 'cherries', 'need', 'knee', 'ground', 'key', 'farm', 'direction', 'crayon', 'authority',
+		'idea', 'cake', 'winter', 'copper', 'son', 'cactus', 'caption', 'road', 'slope', 'trouble', 'finger', 'comparison'];
+	console.log(randomwords);
+}
 
-  window.onload = () => { // just runs start when page opens
+ window.onload = () => { // just runs start when page opens
     start();
-  };
+ };
--- server.improved.js
+++ server.improved.js
@@ -1,39 +1,43 @@
+'use strict';
+
 const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000;
+	fs   = require( 'fs' ),
+	mime = require( 'mime' ),
+	dir  = 'public/',
+	port = 3000;
 
 /*
  *  Game Logic Code Here
  */
 
 const randomwords = ['size','pipe','show','toy','zipper','throne','baby','seat','river','ocean','spade',
-'pump','cakes','skate','cat','vegetable','nut','furniture','tendency','car','sleet','truck','basket','writer',
-'fish','rock','ants','border','experience','kitty','flesh','servant','hydrant','planes','week','office',
-'dog','art','yak','distance','stocking','snails','playground','knot','curtain','wrench','daughter','seashore',
-'side','channel','cow','surprise','team','partner','paper','leg','arch','produce','bell','language','hope',
-'women','angle','cream','jar','respect','pigs','loaf','fly','time','uncle','move','earth','pies','flame',
-'door','place','wing','fact','cherries','need','knee','ground','key','farm','direction','crayon','authority',
-'idea','cake','winter','copper','son','cactus','caption','road','slope','trouble','finger','comparison'];
+		'pump','cakes','skate','cat','vegetable','nut','furniture','tendency','car','sleet','truck','basket','writer',
+		'fish','rock','ants','border','experience','kitty','flesh','servant','hydrant','planes','week','office',
+		'dog','art','yak','distance','stocking','snails','playground','knot','curtain','wrench','daughter','seashore',
+		'side','channel','cow','surprise','team','partner','paper','leg','arch','produce','bell','language','hope',
+		'women','angle','cream','jar','respect','pigs','loaf','fly','time','uncle','move','earth','pies','flame',
+		'door','place','wing','fact','cherries','need','knee','ground','key','farm','direction','crayon','authority',
+		'idea','cake','winter','copper','son','cactus','caption','road','slope','trouble','finger','comparison'];
 
 let scores = [{date: "2020-09-14T19:09:02.410Z", name: "MOM", score: 50, rating: 500}, // filler scores
-              {date: "2020-09-14T19:09:45.849Z", name: "DAD", score: 12, rating: 120},
-              {date: "2020-09-14T19:09:52.299Z", name: "MAD", score: 9, rating: 90},
-              {date: "2020-09-14T19:09:55.471Z", name: "DOG", score: 8, rating: 80}],
-    currentScore = 0,
-    currentWord = '';
+			 {date: "2020-09-14T19:09:45.849Z", name: "DAD", score: 12, rating: 120},
+			 {date: "2020-09-14T19:09:52.299Z", name: "MAD", score: 9, rating: 90},
+			 {date: "2020-09-14T19:09:55.471Z", name: "DOG", score: 8, rating: 80}],
+	currentScore = 0,
+	currentWord = '';
 
 
 const scramble = (word) => {
-  scrambled = word.split('');
-  for (let i = scrambled.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * i)
-    const temp = scrambled[i]
-    scrambled[i] = scrambled[j]
-    scrambled[j] = temp
-  }
-  return scrambled.join('');
+	let scrambled = word.split('');
+
+	for (let i = scrambled.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * i);
+		let temp = scrambled[i];
+		
+		scrambled[i] = scrambled[j];
+		scrambled[j] = temp;
+	}
+	return scrambled.join('');
 };
 
 
@@ -43,112 +47,113 @@ const scramble = (word) => {
  */
 
 const server = http.createServer(function( request,response ) {
-  if (request.method === 'GET') {
-    handleGet(request, response);
-  } else if (request.method === 'POST'){
-    handlePost(request, response);
-  }
+	if (request.method === 'GET') {
+		handleGet(request, response);
+	
+	} else if (request.method === 'POST'){
+		handlePost(request, response);
+	
+	}
 });
 
 const handleGet = function(request, response) {
-  const filename = dir + request.url.slice(1);
+	const filename = dir + request.url.slice(1);
 
-  if (request.url === '/') {
-    sendFile(response, 'public/index.html');
+	if (request.url === '/') {
+		sendFile(response, 'public/index.html');
 
-  } else if (request.url === '/currentWord') {
-    currentWord = randomwords[Math.floor(Math.random() * randomwords.length)];
+	} else if (request.url === '/currentWord') {
+		currentWord = randomwords[Math.floor(Math.random() * randomwords.length)];
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end(scramble(currentWord)); 
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.end(scramble(currentWord)); 
 
-  } else if (request.url === '/getCurrScore') {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end(currentScore.toString());
+	} else if (request.url === '/getCurrScore') {
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.end(currentScore.toString());
 
-  } else if (request.url === '/getScores') {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end(JSON.stringify(scores));
+	} else if (request.url === '/getScores') {
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.end(JSON.stringify(scores));
 
-  } else if (request.url === '/deleteEverything') {
-    scores = [];
-    currentScore = 0;
-    currentWord = '';
+	} else if (request.url === '/deleteEverything') {
+		scores = [];
+		currentScore = 0;
+		currentWord = '';
 
-    response.writeHead(200, "OK");
-    response.end();
+		response.writeHead(200, "OK");
+		response.end();
 
-  } else {
-    sendFile(response, filename);
-  }
+	} else {
+		sendFile(response, filename);
+	}
 };
 
 const handlePost = function(request, response) {
-  let dataString = '';
+	let dataString = '';
 
-  request.on('data', function(data) {
-      dataString += data;
-  })
+	request.on('data', function(data) {
+		dataString += data;
+	});
 
-  if (request.url === '/guess'){
-    request.on( 'end', function() {
-      let guess = JSON.parse(dataString).guess;
-      currentScore = guess === currentWord ? currentScore + 1 : currentScore;
+	if (request.url === '/guess'){
+		request.on( 'end', function() {
+			let guess = JSON.parse(dataString).guess;
+			currentScore = guess === currentWord ? currentScore + 1 : currentScore;
 
-      response.writeHead( 200, "OK", {'Content-Type': 'text/plain'});
-      response.end((guess === currentWord).toString()); // sends the boolean in string format
-    });
+			response.writeHead( 200, "OK", {'Content-Type': 'text/plain'});
+			response.end((guess === currentWord).toString()); // sends the boolean in string format
+		});
 
-  } else if (request.url === '/newScore'){
-    request.on('end', () => {
-      let scoreJSON = JSON.parse(dataString);
+	} else if (request.url === '/newScore'){
+		request.on('end', () => {
+			let scoreJSON = JSON.parse(dataString);
 
-      scoreJSON.score = currentScore;
-      scoreJSON.rating = scoreJSON.score * 10;
+			scoreJSON.score = currentScore;
+			scoreJSON.rating = scoreJSON.score * 10;
 
-      scores.push(scoreJSON);
-      currentScore = 0;
+			scores.push(scoreJSON);
+			currentScore = 0;
 
-      response.writeHead(200, "OK");
-      response.end();
-    });
+			response.writeHead(200, "OK");
+			response.end();
+		});
 
-  } else if (request.url === '/hint'){
-    request.on('end', () => {
-      let hint = parseInt(dataString);
-      let ret = hint < currentWord.length ? currentWord[hint] : '';
+	} else if (request.url === '/hint'){
+		request.on('end', () => {
+			let hint = parseInt(dataString);
+			let ret = hint < currentWord.length ? currentWord[hint] : '';
 
-      response.writeHead(200,  "OK", {'Content-Type': 'text/plain'});
-      response.end(ret.toString());
+			response.writeHead(200,  "OK", {'Content-Type': 'text/plain'});
+			response.end(ret.toString());
 
-    });
-  } else {
-    response.writeHead(200);
-    response.end();
+		});
+	} else {
+		response.writeHead(200);
+		response.end();
 
-  }
+	}
 };
 
 const sendFile = function(response, filename) {
-   const type = mime.getType(filename);
+	const type = mime.getType(filename);
 
-   fs.readFile(filename, function(err, content) {
+	fs.readFile(filename, function(err, content) {
 
-     // if the error = null, then we've loaded the file successfully
-     if (err === null) {
+		 // if the error = null, then we've loaded the file successfully
+		if (err === null) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader(200, {'Content-Type': type});
-       response.end(content);
+			 // status code: https://httpstatuses.com
+			response.writeHeader(200, {'Content-Type': type});
+			response.end(content);
 
-     } else {
+		} else {
+			 // file not found, error code 404
+			response.writeHeader(404);
+			response.end('404 Error: File Not Found');
 
-       // file not found, error code 404
-       response.writeHeader(404)
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
+		}
+	});
 };
 
 server.listen(process.env.PORT || port, () => console.log('Listening on Port:', port));
