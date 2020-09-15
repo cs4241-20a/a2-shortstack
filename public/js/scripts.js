578a58d2cfee8ab5ac8fc972fa7a4bca9e132ffc
GabeAponte <36209249+GabeAponte@users.noreply.github.com>
2020-09-15 15:19:48 -0400

    done

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,97 @@
-// Add some Javascript code here, to run on the front end.
+// Processes the JSON from the server and fills in the body of the HTML table
+const processJSON = (json) => {
+  const tasks = document.getElementById("tasks");
+  tasks.innerHTML = "";
+  json.forEach((task) => {
+    tasks.innerHTML += `
+    <tr>
+      <td>${
+        task.courseName
+      }</td>
+      <td>${
+        task.task
+      }</td>
+      <td>${
+        moment(task.dueDate).format("MM/DD/YYYY, h:mm a")
+      }</td>
+      <td>${
+        task.effort
+      }</td>
+      <td>${
+        task.priority
+      }</td>
+      <td><button class="deleteBtn" id="${task.id}">
+        Delete</button>
+      </td>
+    </tr>
+    `;
+  });
+}
 
-console.log("Welcome to assignment 2!")
+const submit = function(e) {
+  // prevent default form action from being carried out
+  e.preventDefault()
+
+  const input = document.querySelector('#courseName');
+  const input2 = document.querySelector('#task');
+  const input3 = document.querySelector('#dueDate');
+  const input4 = document.querySelector('#effort');
+
+  // alert the user tnat they need to specify all the data before adding a task
+  if(input.value.toString().trim() == '' || input2.value.toString().trim() == '' || input3.value.toString().trim() == '' || input4.value.toString().trim() == '' ){
+    alert("Please fill in all the fields");
+    return false;
+  }
+
+  const json = {courseName: input.value, task : input2.value, dueDate : input3.value, effort : input4.value},
+  body = JSON.stringify(json)
+
+  fetch('/submit', {
+    method:'POST',
+    body
+  })
+  .then(response => response.json())
+  .then(json => processJSON(json));
+
+  // clear the input values now that we have added the inputs to the table
+  document.getElementById("courseName").value = "";
+  document.getElementById("task").value = "";
+  document.getElementById("dueDate").value = "";
+  document.getElementById("effort").value = "1";
+
+  return false;
+}
+
+//Delete function that POSTS a json object with delete and the id of the row to the server
+const deleteTask = function (e) {
+  e.preventDefault();
+  const id = e.target.getAttribute("id");
+
+  const json = {delete: 'delete', id},
+  body = JSON.stringify(json);
+
+  fetch("/submit", {
+    method: "POST",
+    body,
+  })
+  .then(response => response.json())
+  .then(json => processJSON(json));
+
+  return false;
+}
+
+window.onload = function() {
+  const addBtn = document.getElementById('addBtn')
+  addBtn.onclick = submit;
+
+  fetch('/appData')
+  .then(response => response.json())
+  .then(json => processJSON(json));
+
+  // onClickListeners to add the delete functionality to all the delete buttons
+  document.addEventListener("click", function (e) {
+    if (e.target && e.target.classList[0] == "deleteBtn") {
+      deleteTask(e);
+    }
+  });
+}
--- server.improved.js
+++ server.improved.js
@@ -1,31 +1,40 @@
 const http = require( 'http' ),
-      fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+fs   = require( 'fs' ),
+// IMPORTANT: you must run `npm install` in the directory for this assignment
+// to install the mime library used in the following line of code
+mime = require( 'mime' ),
+moment = require('moment'),
+dir  = 'public/',
+port = 3000
+
+var id = 1; // will be used to add unique id to each JSON data object
 
 const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
+  {'courseName': 'CS4241 - Webware', 'task': 'Assignment 2 - TODO List', 'dueDate': '2020-09-16T23:59', 'effort' : '4', 'priority' : 'Completed!!!', 'id' : '1'}
 ]
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+    handleGet( request, response )
+  }
+  else if( request.method === 'POST' ){
+    handlePost( request, response )
   }
 })
 
 const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+  const filename = dir + request.url.slice( 1 )
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  }
+  // send the appdata when asked
+  else if (request.url === '/appData'){
+    response.writeHead(200, "OK", {'Content-Type': 'application/json'})
+    response.write(JSON.stringify(appdata));
+    response.end()
+  }
+  else{
     sendFile( response, filename )
   }
 }
@@ -34,39 +43,91 @@ const handlePost = function( request, response ) {
   let dataString = ''
 
   request.on( 'data', function( data ) {
-      dataString += data 
+    dataString += data
   })
 
   request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+    var postedData = JSON.parse(dataString); // the data recieved from a post
 
-    // ... do something with the data here!!!
+    // check if the post JSON object contains a deletion request
+    if(postedData.hasOwnProperty('delete')){
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+      //find the correct JSON object in appdata via the unique id and then remove that index to delete the proper data
+      appdata.splice(appdata.findIndex(x => x.id.toString() === postedData.id.toString()), 1);
+      response.writeHead(200, "OK", {'Content-Type': 'application/json'})
+      response.write(JSON.stringify(appdata));
+      response.end();
+      return false;
+    }
+
+    // we are not deleting, so add the priority field and calculate its value
+    var priority = calculatePriority(postedData.dueDate, postedData.effort);
+    postedData.priority = priority;
+
+    // assign the data an id for deletion refrence
+    id++;
+    postedData.id = id;
+
+    // calculates the priority based off hard coded inputs (number of days before due and effort determines the priority)
+    function calculatePriority (dueDate, effort) {
+      var inputDate = new Date(moment(dueDate).format("MM/DD/YYYY"));
+      var now = new Date(moment().format("MM/DD/YYYY"));
+      var daysBeforeDue = (inputDate.getTime() - now.getTime()) / (1000 * 3600 * 24);
+
+      if(daysBeforeDue >= 14) return "Low";
+      if(daysBeforeDue >= 7 && (effort == '1' || effort == '2')) return "Low";
+      if(daysBeforeDue >= 7 && effort == '3') return "Medium-Low";
+      if(daysBeforeDue >= 7 && effort == '4') return "Medium";
+      if(daysBeforeDue >= 7 && effort == '5') return "Medium-High";
+      if(daysBeforeDue < 0) return "PAST DUE";
+      if(daysBeforeDue == 0 && effort == '5') return "I'm So Screwed";
+      if(daysBeforeDue == 0) return "High";
+      if((daysBeforeDue == 1) && (effort == '5' || effort == '4')) return "High";
+      if((daysBeforeDue == 1) && (effort == '3' || effort == '2' || effort == '1')) return "Medium-High";
+      if((daysBeforeDue == 2) && (effort == '5' || effort == '4')) return "High";
+      if((daysBeforeDue == 2) && (effort == '3')) return "Medium-High";
+      if((daysBeforeDue == 2) && (effort == '2' || effort == '1')) return "Medium";
+      if(daysBeforeDue == 3 && (effort == '5' || effort == '4')) return "High";
+      if(daysBeforeDue == 3 && (effort == '3' || effort == '2')) return "Medium";
+      if(daysBeforeDue == 3 && (effort == '1')) return "Medium-Low";
+      if((daysBeforeDue == 4 || daysBeforeDue == 5) && (effort == '5')) return "High";
+      if((daysBeforeDue == 4 || daysBeforeDue == 5) && (effort == '4')) return "Medium-High";
+      if((daysBeforeDue == 4 || daysBeforeDue == 5) && (effort == '3')) return "Medium";
+      if((daysBeforeDue == 4 || daysBeforeDue == 5) && (effort == '2')) return "Medium-Low";
+      if((daysBeforeDue == 4 || daysBeforeDue == 5) && (effort == '1')) return "Low";
+      if(daysBeforeDue == 6 && (effort == '5' || effort == '4')) return "Medium-High";
+      if(daysBeforeDue == 6 && (effort == '3' || effort == '2')) return "Medium-low";
+      if(daysBeforeDue == 6 && (effort == '1')) return "Low";
+      else return "Cannot Decide"
+    }
+
+    appdata.push(postedData);
+    response.writeHead(200, "OK", {'Content-Type': 'application/json'})
+    response.write(JSON.stringify(appdata));
     response.end()
   })
 }
 
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+  const type = mime.getType( filename )
 
-   fs.readFile( filename, function( err, content ) {
+  fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+    // if the error = null, then we've loaded the file successfully
+    if( err === null ) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+      // status code: https://httpstatuses.com
+      response.writeHeader( 200, { 'Content-Type': type })
+      response.end( content )
 
-     }else{
+    }else{
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+      // file not found, error code 404
+      response.writeHeader( 404 )
+      response.end( '404 Error: File Not Found' )
 
-     }
-   })
+    }
+  })
 }
 
 server.listen( process.env.PORT || port )
