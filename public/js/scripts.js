70e9653c843c9f44b4baa113833d3c28d592c64f
Isabel Alvarado Blanco Uribe <alvaradoblancouribe@gmail.com>
2020-09-15 01:38:41 -0400

    first edition of assigment

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,129 @@
 // Add some Javascript code here, to run on the front end.
+const deleteData= function(e){
+  fetch('/delete',{
+    method:'POST'
+  })
+  .then( function( response ) {
+    console.log("deleted successfully")
+  })
+  return false;
+}
+const showData = function( e ){
+   //e.preventDefault()
 
-console.log("Welcome to assignment 2!")
+    fetch('/showData',{
+      method:'GET'
+    })
+    .then( response=> response.json())
+    .then(json=>{
+      for(var jsonItem in json.tasks){
+      jsonItem = Number(jsonItem)
+      jsonUpdated=json.tasks[jsonItem]
+      var list = document.createElement("li")
+      var h6 = document.createElement("b");
+      var li = document.createElement("p");
+      let dateTitle =document.createTextNode("Date created:")
+      let date= document.createTextNode(jsonUpdated.date)
+      let taskTitle =document.createTextNode("Task: ")
+      let task = document.createTextNode(jsonUpdated.task)
+      let priorityTitle= document.createTextNode("Priority: ")
+      let priority= document.createTextNode(jsonUpdated.priority)
+      let expectedTitle= document.createTextNode("Hours: ")
+      let expected=document.createTextNode(jsonUpdated.expected+" hours")
+      h6.appendChild(dateTitle);
+      li.appendChild(date);
+      list.appendChild(h6);
+      list.appendChild(li);
+      h6 = document.createElement("b");
+      li = document.createElement("p");
+      h6.appendChild(taskTitle);
+      li.appendChild(task);
+      list.appendChild(h6);
+      list.appendChild(li);
+      h6 = document.createElement("b");
+      li = document.createElement("p");
+      h6.appendChild(priorityTitle);
+      li.appendChild(priority);
+      list.appendChild(h6);
+      list.appendChild(li);
+      h6 = document.createElement("b");
+      li = document.createElement("p");
+      h6.appendChild(expectedTitle);
+      li.appendChild(expected);
+      list.appendChild(h6);
+      list.appendChild(li);
+      document.getElementById("tasks").appendChild(list);
+    }})
+
+    return false
+  }
+
+  const submit = function( e ) {
+    // prevent default form action from being carried out
+    e.preventDefault()
+
+    const taskName = document.querySelector( '#taskName' ),
+          expectedHours = document.querySelector('#expected'),
+          json = { task: taskName.value, expected: expectedHours.value},
+          body = JSON.stringify( json )
+
+    fetch( '/submit', {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: body 
+    })
+   .then( response=> response.json())
+   .then(json=>{
+      //    response.json().then(json =>{
+      // do something with the reponse 
+      jsonUpdated=json
+      var list = document.createElement("li")
+      var h6 = document.createElement("b");
+      var li = document.createElement("p");
+      let dateTitle =document.createTextNode("Date created:")
+      let date= document.createTextNode(jsonUpdated.date)
+      let taskTitle =document.createTextNode("Task: ")
+      let task = document.createTextNode(jsonUpdated.task)
+      let priorityTitle= document.createTextNode("Priority: ")
+      let priority= document.createTextNode(jsonUpdated.priority)
+      let expectedTitle= document.createTextNode("Hours: ")
+      let expected=document.createTextNode(jsonUpdated.expected+" hours")
+      h6.appendChild(dateTitle);
+      li.appendChild(date);
+      list.appendChild(h6);
+      list.appendChild(li);
+      h6 = document.createElement("b");
+      li = document.createElement("p");
+      h6.appendChild(taskTitle);
+      li.appendChild(task);
+      list.appendChild(h6);
+      list.appendChild(li);
+      h6 = document.createElement("b");
+      li = document.createElement("p");
+      h6.appendChild(priorityTitle);
+      li.appendChild(priority);
+      list.appendChild(h6);
+      list.appendChild(li);
+      h6 = document.createElement("b");
+      li = document.createElement("p");
+      h6.appendChild(expectedTitle);
+      li.appendChild(expected);
+      list.appendChild(h6);
+      list.appendChild(li);
+      document.getElementById("tasks").appendChild(list);
+      //.then( function( response ) {
+
+   })//);
+    return false
+  };
+  //when everything on your page has loaded 
+  window.onload = function() {
+    const tasks = document.getElementById( 'tasks' )
+    tasks.onload = showData();
+    const button = document.querySelector( 'button' )
+    button.onclick = submit
+    const deleteButton= document.getElementById('delete')
+    deleteButton.onclick = deleteData
+  }
--- server.improved.js
+++ server.improved.js
@@ -6,12 +6,6 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
-
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
     handleGet( request, response )    
@@ -20,31 +14,102 @@ const server = http.createServer( function( request,response ) {
   }
 })
 
+const readData = fs.readFileSync('tasks.json','utf8')
+const appdata= JSON.parse(readData)
+
+const updateFile= function(){
+  fs.writeFile('tasks.json', JSON.stringify(appdata), (error) => { 
+    // In case of a error throw err exception. 
+    if (error) throw err; 
+  }) 
+}
+// calculates the priority of a task given the expected hours of work
+const calculatePriority= function (expected){
+  var expected = Number(expected)
+  let priority="no priority"
+  if (expected>5){
+    priority= "high"
+  }
+  else if (expected >2){
+    priority = "medium"
+  }
+  else{
+    priority="low"
+  }
+  return priority
+}
+//handles deleting the last entry
+const deleteData = function(request, response){
+  appdata.tasks.pop()
+  updateFile()
+  response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+  response.end()
+}
+
+//handles updating new data 
+const updateData = function(request, response){
+  let jsonData;
+  request.on('data', function(data){
+    jsonData= JSON.parse(data);
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    jsonData["date"]=date
+    let priority = calculatePriority(jsonData.expected)
+    jsonData["priority"]= priority
+    appdata.tasks.push(jsonData)
+    updateFile()
+  })
+  request.on( 'end', function() {
+    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
+    console.log(appdata)
+    response.write(JSON.stringify(jsonData))
+    response.end()
+  })
+}
+const handlePost = function( request, response ) {
+
+  if (request.url === '/submit'){
+    updateData(request,response);
+  }
+  else if (request.url==='/delete'){
+    console.log("delete is working somewhat")
+    deleteData(request,response);
+  }
+  else{
+    let dataString = ''
+
+    request.on( 'data', function( data ) {
+        console.log(data)
+        dataString += data 
+    })
+
+    request.on( 'end', function() {
+      console.log( "parse "+JSON.parse( dataString ) )
+
+      // ... do something with the data here!!!
+      response.writeHead( 200, "OK", {'Content-Type': 'application/json' })
+      response.write(appdata)
+      response.end()
+      //response.end(dataString)
+    })
+  }
+}
+
 const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 ) 
 
   if( request.url === '/' ) {
     sendFile( response, 'public/index.html' )
-  }else{
+  }
+  else if(request.url === '/showData'){
+    console.log(appdata)
+    console.log("sending file")
+    sendFile(response, 'tasks.json')
+  }
+  else{
+    console.log(request.url)
     sendFile( response, filename )
   }
-}
-
-const handlePost = function( request, response ) {
-  let dataString = ''
-
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
-
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    // ... do something with the data here!!!
-
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
 }
 
 const sendFile = function( response, filename ) {
@@ -57,12 +122,12 @@ const sendFile = function( response, filename ) {
 
        // status code: https://httpstatuses.com
        response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+       response.end(content)
 
      }else{
 
        // file not found, error code 404
-       response.writeHeader( 404 )
+       response.writeHeader( 404)
        response.end( '404 Error: File Not Found' )
 
      }
