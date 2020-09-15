e1543d9e13ba89454ff2cbd21445b291dc507fca
Alex Simoneau <afsimoneau@gmail.com>
2020-09-14 23:41:48 -0400

    still not doing useful stuff yet

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,34 @@
-// Add some Javascript code here, to run on the front end.
+const loadTable = function(jsonData){
 
-console.log("Welcome to assignment 2!")
+}
+
+const submit = function (e) {
+  // prevent default form action from being carried out
+  e.preventDefault();
+
+  const inputRoute = document.querySelector("#inputRoute"),
+    inputTime = document.querySelector("#inputTime"),
+    inputDistance = document.querySelector("#inputDistance"),
+    json = {
+      route: inputRoute.value,
+      time: inputTime.value,
+      distance: inputDistance.value,
+      pace: "",
+    },
+    body = JSON.stringify(json);
+
+  fetch("/submit", {
+    method: "POST",
+    body,
+  }).then(function (response) {
+    // do something with the reponse
+    loadTable(JSON.parse(response))
+  });
+
+  return false;
+};
+
+window.onload = function () {
+  const button = document.querySelector("button");
+  button.onclick = submit;
+};
