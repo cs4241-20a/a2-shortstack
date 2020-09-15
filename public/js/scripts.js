b6d88fd1b3ac8be2de049e025fb285fbae320ef3
Kirsten Hart <kirstenhart@Kirstens-MacBook-Pro.local>
2020-09-14 20:13:50 -0400

    updates html, js, and server

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,32 @@
-// Add some Javascript code here, to run on the front end.
+const addPlayer = function( e ) {
+  // prevent default form action from being carried out
+  e.preventDefault()
 
-console.log("Welcome to assignment 2!")
+  const number = document.querySelector( '#number' ),
+        firstName = document.querySelector( '#firstName' ),
+        lastName = document.querySelector( '#lastName' ),
+        goals = document.querySelector( '#goals' ),
+        table = document.querySelector( '#resultTable'),
+        assists = document.querySelector( '#assists' ),
+        json = { number: number.value, firstName: firstName.value, lastName: lastName.value, goals: goals.value, assists: assists.value },
+        body = JSON.stringify( json );
+
+  fetch( '/addPlayer', {
+    method:'POST',
+    body 
+  })
+  .then( function( response ) {
+    return response.text()
+  })
+  .then( function(txt){
+      console.log(txt)
+      updateRoster(table, JSON.parse(txt))
+  })
+
+  return false
+}
+
+window.onload = function() {
+  const button = document.querySelector( 'button' )
+  button.onclick = addPlayer
+}
