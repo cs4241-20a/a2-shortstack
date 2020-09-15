304e768fe5a9fd9ae8bd995babc9c129855a31c3
jcybul <jcybul@wpi.edu>
2020-09-15 13:55:11 -0400

    finalized design and fuctionality

--- server.improved.js
+++ server.improved.js
@@ -33,6 +33,25 @@ const handleGet = function( request, response ) {
   }
 }
 
+
+//estimate the time it took based on fish size and line class
+function time(weight,line){
+  p = weight;
+  l = line;
+  return (p/l)/2;
+}
+
+var curday = function(sp){
+  today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //As January is 0.
+  var yyyy = today.getFullYear();
+
+  if(dd<10) dd='0'+dd;
+  if(mm<10) mm='0'+mm;
+  return (mm+sp+dd+sp+yyyy);
+};
+
 const handlePost = function( request, response ) {
   let dataString = ''
 
@@ -42,10 +61,11 @@ const handlePost = function( request, response ) {
   request.on( 'end', function() {
     console.log( JSON.parse( dataString ))
     let parsedData = JSON.parse(dataString)
+    parsedData['date'] = curday('/');
+    parsedData['time'] = time(parsedData.fishweight,parsedData.lineclass);
     appdata.push(parsedData);
     response.writeHead( 200, "OK", {'Content-Type': 'application/json' })
-    response.data(JSON.stringify(appdata))
-    response.end()
+    response.end(JSON.stringify(parsedData));
   })
 }
 
