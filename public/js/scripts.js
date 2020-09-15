2dc5132b6133830ddca9ee1bf29413dda54664c7
unknown <jstoessel@wpi.edu>
2020-09-14 23:34:36 -0400

    Added small button interactions.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,7 @@
 // Add some Javascript code here, to run on the front end.
+var clickcount = 0;
+document.getElementById('clickbtn').style.display="none";
+document.getElementsByClassName('postgame').style.display="none";
 
 console.log("Welcome to assignment 2!")
 
@@ -22,6 +25,34 @@ const submit = function( e ) {
 
     return false
   }
+
+  //when start button is clicked swap visibilities and start 30 second timer.
+  function start() {
+    document.getElementById('clickbtn').style.display="block";
+    document.getElementById('startbtn').style.display="none";
+    setTimeout(end(), 3000)
+  }
+
+  //increments number of clicks by 1 per click.
+  function add() {
+    clickcount += 1;
+    document.getElementById('currentclicks').innerHTML = stringify(clickcount) + "Points";
+  }
+
+  //after 30 second timer is elapsed disable button and allow recording.
+  function end() {
+    document.getElementById('clickbtn').style.display="none";
+    document.getElementsByClassName('postgame').style.display="block";
+
+    //for loop here for checking if high score.
+    //if high score: alert("Congratulations! You've bested the high score!")
+
+    //else:
+    alert("You've run out of time!");
+  }
+
+  
+
 
   window.onload = function() {
     const button = document.querySelector( 'button' )
