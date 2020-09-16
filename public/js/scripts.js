a42742254d0da21f2260c3b069acde54c77e4b53
Nicole Conill <nconill@wpi.edu>
2020-09-15 20:42:06 -0400

    Update 2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,9 @@
 // Register HTML Elements
 const addBtn = document.getElementById('add');
 const current = document.getElementById('current');
+const newentry = document.getElementById('newentry');
+
+let tabularFront;
 
 const create = function(element) {
 	return document.createElement(element);
@@ -15,12 +18,10 @@ const headings = function() {
 	let th2 = create('th')
 	let th3 = create('th')
 	let th4 = create('th')
-	let th5 = create('th')
 	th1.innerHTML = "Name";
-	th2.innerHTML = "Bike Year";
+	th2.innerHTML = "Bike Number";
 	th3.innerHTML = "Bike Brand";
-	th4.innerHTML = "Bike CC's";
-	th5.innerHTML = "Class"
+	th4.innerHTML = "Class"
 
 	let tr = create('tr');
 
@@ -28,6 +29,116 @@ const headings = function() {
 	append(tr, th2);
 	append(tr, th3);
 	append(tr, th4);
-	append(tr, th5);
 	append(current, tr);
 };
+
+const refresh = function() {
+	fetch('/refresh', {
+		method: 'GET'
+	}).then(function (response) {
+		return response.json();
+	}).then(function (data) {
+		tabularFront = data;
+		current.innerHTML = "";
+		headings();
+
+		let rowCount = 1;
+		tabularFront.map(function (row)) {
+			let tr = create('tr');
+			let td1 = create('th');
+			let td2 = create('th');
+			let td3 = create('th');
+			let td4 = create('th');
+			let td5 = create('th');
+		};
+
+		td1.innerHTML = row.name;
+		td2.innerHTML = row.bikenumber;
+		td3.innerHTML = row.bikebrand;
+		td4.innerHTML = row.class;
+
+		append(tr, td1);
+		append(tr, td2);
+		append(tr, td3);
+		append(tr, td4);
+
+		append(current, tr);
+
+		tr.className = rowCount;
+		rowCount++;
+
+	});
+};
+refresh();
+
+
+let input;
+let modifyIndex = 0;
+
+const makeBody = function () {
+	const name = document.querySelector('#name');
+	const bikeyear = document.querySelector('#bikeyear');
+
+	const bikebrandTable = document.getElementById('bikebrand');
+	let bikebrand;
+	for (let i = 0; i < bikebrandTable.length; i++) {
+		if (bikebrandTable[i].checked) {
+			bikebrand = bikebrandTable[i].value;
+			break
+		}
+	}
+
+	const json = {
+		name: name.value,
+		bikeyear: bikeyear.value,
+		bikebrand: bikebrand,
+		modifyIndex
+	};
+
+	return JSON.stringify(json);
+};
+
+const handlePost = function () {
+	let body = makeBody();
+	let checkBody = JSON.parse(body);
+	let hint = document.getElementById('hint');
+
+	if (checkBody['name'] === ""
+		|| checkBody['bikenumber'] === ""
+		|| checkBody['bikebrand'] === "") {
+		hint.innerHTML = "There are missing fields!";
+	} else {
+		hint.innerHTML = "";
+		fetch(`/${input}`, {
+			method: 'POST',
+			body
+		}).then(function (response) {
+			console.log("Post sent to server: " + response);
+			refresh();
+		});
+	}	
+}
+
+const setInput = function (e) {
+    if (addBtn.innerHTML === "Submit") {
+        input = 'add';
+        handlePost();
+    } else {
+        input = 'modify';
+        handlePost();
+        rightHead.innerHTML = "New Race Entry";
+        addBtn.innerHTML = "Submit";
+
+        document.querySelector('#name').value = "";
+        document.querySelector('#bikenumber').value = "";
+
+        let bikebrandS = document.getElementsByName('bikebrand');
+        for (let i = 0; i < bikebrandS.length; i++) {
+            bikebrandS[i].checked = false;
+        }
+
+    }
+    e.preventDefault();
+    return false;
+};
+addBtn.onclick = setInput;
--- server.improved.js
+++ server.improved.js
@@ -1,66 +1,125 @@
+// server -----------------------------------------------------------
+
 const http = require( 'http' ),
 fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000;
+mime = require( 'mime' ),
+dir  = 'public/',
+port = 3000;
 
-      const server = http.createServer( function( request,response ) {
-        if( request.method === 'GET' ) {
-          handleGet( request, response )    
-        }else if( request.method === 'POST' ){
-          handlePost( request, response ) 
-        }
-      })
+const server = http.createServer( function( request,response ) {
+  if( request.method === 'GET' ) {
+    handleGet( request, response )    
+  }else if( request.method === 'POST' ){
+    handlePost( request, response ) 
+  }
+});
 
-      const handleGet = function( request, response ) {
-        const filename = dir + request.url.slice( 1 ) 
+const sendFile = function( response, filename ) {
+  const type = mime.getType( filename ) 
 
-        if( request.url === '/' ) {
-          sendFile( response, 'public/index.html' )
-        }else{
+  fs.readFile( filename, function( err, content ) {
 
-        }
-      }
+    // if the error = null, then we've loaded the file successfully
+    if( err === null ) {
 
-      const handlePost = function( request, response ) {
-        let dataString = ''
+    // status code: https://httpstatuses.com
+    response.writeHeader( 200, { 'Content-Type': type })
+    response.end( content )
 
-        request.on( 'data', function( data ) {
-          dataString += data 
-        })
+  }else{
 
-        request.on( 'end', function() {
-          console.log( JSON.parse( dataString ) )
+    // file not found, error code 404
+    response.writeHeader( 404 )
+    response.end( '404 Error: File Not Found' )
 
-    // ... do something with the data here!!!
+  }
+})
+};
+
+server.listen( process.env.PORT || port )
+
+
+// Example Data -----------------------------------------------------
+const tabularBack = [
+{'name': 'Nicole Conill', 'bikenumber': 13, 'bikebrand': 'KTM', 'class':'female', 'fee': 35}
+];
+
+
+// Send Data, Add/Delete/Edit Rows ----------------------------------
+const sendData = function(response, dataset) {
+  const type = mime.getType(dataset);
+  response.writeHead(200, {'Content-Type': type});
+  response.write(JSON.stringify(dataset));
+  response.end()
+};
+
+const calculateFee = function(row) {
+  let fee = 0;
+  fee = fee + 35;
+  return fee;
+};
+
+const addRow = function (str) {
+  let json = JSON.parse(str);
+  tabularBack.push(json);
+
+  for (let i = 0; i < tabularBack.length-1; i++) {
+    let row = tabularBack[i];
+    row['fee'] = calculateFee(row);
+  }
+};
+
+const deleteRow = function (str) {
+  for (let i = 0; i < tabularBack.length; i++) {
+    if ((i+1).toString() === str[4]) tabularBack.splice(i,1);
+  }
+};
+
+const editRow = function (str) {
+  let json = JSON.parse(str);
+  for (let i = 0; i < tabularBack.length; i++) {
+    if ((i+1).toString() === json['modifyIndex']) {
+      let row = tabularBack[i];
+      row['name'] = json['name'];
+      row['bikenumber'] = json['bikenumber'];
+      row['bikebrand'] = json['bikebrand'];
+      row['class'] = json['class'];
+      row['fee'] = calculateFee(row);
+    }
+  }
+};
+
+
+// handleGet & handlePost -------------------------------------------
+const handleGet = function( request, response ) {
+  const filename = dir + request.url.slice( 1 ) 
+
+  if( request.url === '/' ) {
+    sendFile( response, 'public/index.html' )
+  } else if (request.url === '/refresh') {
+    sendData(response, tabularBack);
+  } else {
+    sendFile(response, filename);
+  }
+};
+
+const handlePost = function( request, response ) {
+  let dataString = ''
+  request.on( 'data', function( data ) {
+    dataString += data 
+  });
+
+  request.on( 'end', function() {
+    if (request.url === '/add') {
+      addRow(dataString);
+    } else if (request.url === '/delete') {
+      deleteRow(dataString);
+    } else if (request.url === '/edit') {
+      editRow(dataString);
+    }
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
     response.end()
   })
-      }
-
-      const sendFile = function( response, filename ) {
-       const type = mime.getType( filename ) 
-
-       fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-     }
-
-     server.listen( process.env.PORT || port )
+}
+// ------------------------------------------------------------------
