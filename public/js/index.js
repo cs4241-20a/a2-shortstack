77f83d949a20ffb80afd2eae81acb4132c5ff277
TweedChristian <christiantweed@gmail.com>
2020-09-16 05:21:44 -0400

    Moved files, updated server, added card stack, added dynamic loading of cats, fixed a bunch of styling

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,16 @@
-//Method: Ping db for list of images
+const catIndexRoute = '/api/cats',
+      catUpdateRoute = '/api/updateCat/',
+      catDeleteRoute = '/api/deleteCat/',
+      catNewRoute = '/api/newCat'
+
+let defaultCatList
+let loadingMessage
+let userCatList
+let userCatListHeading
+let cats 
+
+//Enabling Use-Strict Mode
+'use-strict'
 
 const submit = function( e ) {
     // prevent default form action from being carried out
@@ -18,11 +30,94 @@ const submit = function( e ) {
     })
 
     return false
-  }
+}
 
-  window.onload = function() {
+window.onload = function() {
+    defaultCatList = document.getElementById("catList")
+    loadingMessage = document.getElementById("loadingMessage")
+    userCatListHeading = document.getElementById("userCatHeading")
+    userCatList = document.getElementById("userCatList")
     // const button = document.querySelector( 'button' )
     // button.onclick = submit
-    
     const apiUrl = "/api/cats"
-  }
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(cats => {
+        loadCats(cats)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+const loadCats = (apiCats) => {
+    cats = apiCats
+    const default_cats = cats.default_cats
+    default_cats.map((cat) => {
+        createCatCard(cat, defaultCatList, false)
+    })
+    const user_cats = apiCats.user_cats
+    if(user_cats.length > 0){
+       createUserCatList(user_cats)
+    }
+}
+
+const createCatCard = (cat, catList, includeButton) => {
+   const catCard = document.createElement("article")
+   catCard.className = "c_catCard"
+
+   const catCardImage = new Image()
+   catCardImage.src = cat.imageUrl
+   catCardImage.alt = `A picture of a cat named ${cat.name}`
+   catCardImage.loading = 'lazy'
+   catCardImage.className = 'c_catCard__catImage'
+
+   const catCardHeader = document.createElement("header")
+   catCardHeader.className = "c_catCard__catHeader"
+
+   const catCardName = document.createElement("h4")
+   catCardName.innerText = cat.name
+   catCardName.className = "u_txt--hdg4 c_catCard__catName"
+
+   const catCardInformation = document.createElement("section")
+   catCardInformation.className = "c_catCard__catInformation"
+
+   const catCardDescription = document.createElement("p")
+   catCardDescription.className = "u_txt c_catCard__description"
+   catCardDescription.innerText = cat.description   
+
+   catCardInformation.appendChild(catCardDescription)
+   if(includeButton){
+    const catCardButton = document.createElement("button")
+    catCardButton.className = "c_catList__secondaryButton"
+    catCardButton.innerText = "Update Cat"
+    catCardButton.id = `${cat.name}`
+    catCardButton.onclick = () => {
+        updateCatButton(catCardButton.id)
+    }
+    catCardInformation.appendChild(catCardButton)
+   }
+   catCardHeader.appendChild(catCardName)
+   catCard.appendChild(catCardImage)
+   catCard.appendChild(catCardHeader)
+   catCard.appendChild(catCardInformation)
+
+   catList.appendChild(catCard)
+   loadingMessage.style = "display: none;"
+}
+
+const createUserCatList = (cats) => {
+    userCatListHeading.style = "display: visible;"
+    cats.map((cat) => {
+        createCatCard(cat, userCatList, true)
+    })
+}  
+
+const updateCatButton = (name) => {
+    console.log(findCatByName(name))
+}
+
+const findCatByName = (name) => {
+    return cats.user_cats.filter((cat) => cat.name === name)
+}
--- server.js
+++ server.js
@@ -1,25 +1,98 @@
 const http = require( 'http' ),
       fs   = require( 'fs' ),
-      // IMPORTANT: you must run `npm install` in the directory for this assignment
-      // to install the mime library used in the following line of code
       mime = require( 'mime' ),
       dir  = 'public/',
-      port = 3000
+      port = 3000,
+      dbLocation = dir + 'assets/cats.json',
+      indexFile = dir + 'index.html'
+
+//Enabling Use-Strict Mode
+'use-strict'
+
+//Initializing Cache
+const memoizedFs = (response) => {
+  let cache = {};
+  return (fileName) => {
+    //TODO: Check for cat image
+    const type = mime.getType(fileName) 
+    if(fileName in cache) {
+      let fileString = cache[fileName]
+      if(!!response){
+        response.writeHeader( 200, { 'Content-Type': type })
+        response.end(fileString)
+      }
+    }
+    fs.readFile(fileName, (err, fileContent) => {
+      if(err){
+        if(!!response){
+          response.writeHeader( 404 )
+          response.end( '404 Error: File Not Found' )
+        }
+        else{
+          console.log(err);
+          console.error("Loaded a private file incorrectly")
+        }
+      }
+      else {
+        cache[fileName] = fileContent
+        if(!!response) {
+          response.writeHeader( 200, { 'Content-Type': type })
+          response.end(fileContent);
+        }
+      }
+    });
+  }
+}
+
+const sendCacheFile = (response, fileName) => {
+  memoizedFs(response)(fileName);
+}
+
+const loadCacheFile = (fileName) => {
+  memoizedFs()(fileName);
+}
+
+//Preliminary Loading of DB
+loadCacheFile(dbLocation)
+loadCacheFile(indexFile)
 
 const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
+  if(request.url.includes('api')){
+    readAPICall(request, response);
+  }
+  else if( request.method === 'GET' ) {
     handleGet( request, response )    
   }else if( request.method === 'POST' ){
     handlePost( request, response ) 
   }
 })
 
+const readAPICall = function(request, response) {
+  const apiInformation = request.url.slice(1).split('/').slice(1)
+  const apiCall = apiInformation[0]
+  switch(apiCall) {
+    case "cats":
+      sendCacheFile(response, dbLocation)      
+      break;
+    case "updateCat":
+      //Patch Request
+    case "deleteCat":
+      //Post
+    case "newCat":
+      //Post
+    default:
+      response.writeHead( 403, "Forbidden", {'Content-Type': 'application/json' })
+      response.end(JSON.stringify({error: "Not a valid API call"}))
+      break;
+  }
+}
+
 const handleGet = function( request, response ) {
   const filename = dir + request.url.slice( 1 ) 
   if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
+    sendCacheFile( response, indexFile )
   } else{
-    sendFile( response, filename )
+    sendCacheFile( response, filename )
   }
 }
 
@@ -39,69 +112,5 @@ const handlePost = function( request, response ) {
     response.end()
   })
 }
-
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
-
-   fs.readFile( filename, function( err, content ) {
-
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
-
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
-
-     }else{
-
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
-
-     }
-   })
-}
-
-//Based off of https://www.freecodecamp.org/news/understanding-memoize-in-javascript-51d07d19430e
-const memoizedFs = (response) => {
-  let cache = {};
-  return (fileName) => {
-    if(fileName in cache) {
-      let fileString = cache[fileName];
-      if(!!response){
-        response.end(fileString);
-      }
-    }
-    fs.readFile(fileName, (err, data) => {
-      if(err){
-        if(!!cache['error.html'] && !!response){
-          response.end(cache['error.html'], 'utf-8');
-        }
-        else if(!!response){
-          //If for some reason fs never loaded the error response
-          response.end( '404 Error: File Not Found' )
-        }
-        else{
-          //Do nothing
-        }
-      }
-      else {
-        cache[fileName] = data;
-        if(!!response) {
-          response.end(data);
-        }
-      }
-    });
-  }
-}
-
-const sendCacheFile = (response, fileName) => {
-  memoizedFs(response)(fileName);
-}
-
-const loadCacheFile = (fileName) => {
-  memoizedFs()(fileName);
-}
-
 
 server.listen( process.env.PORT || port )
