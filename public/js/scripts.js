d4e5ee05dedc6e8f0fd2a6a6268596665dac78c1
Alex Simoneau <afsimoneau@gmail.com>
2020-09-16 15:32:12 -0400

    major changes from glitch. copy pasted over

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,35 +1,69 @@
-const loadTable = function (responseData) {
-  var jsonData = JSON.parse(responseData);
-  console.log(jsonData);
+const loadTable = function(jsonData) {
+  console.log(jsonData)
+  var table = document.getElementById("resultTableBody");
+  table.innerHTML = "";
+  for (var i = 0; i < jsonData.length; i++) {
+    var tr = table.insertRow(-1);
+    for (var j in jsonData[i]) {
+      var tb = tr.insertCell(-1);
+      tb.innerHTML = jsonData[i][j];
+    }
+  }
 };
 
-const submit = function (e) {
+const submit = function(e) {
   // prevent default form action from being carried out
   e.preventDefault();
 
-  const inputRoute = document.querySelector("#inputRoute"),
-    inputTime = document.querySelector("#inputTime"),
-    inputDistance = document.querySelector("#inputDistance"),
-    json = {
+  //sanitize inputs here
+
+  var inputRoute = document.querySelector("#inputRoute");
+  var inputTime = document.querySelector("#inputTime");
+  var inputDistance = document.querySelector("#inputDistance");
+  if (inputRoute.value === "") {
+    alert("Please input a route");
+    return false;
+  }
+  if (inputTime.value === "") {
+    alert("Please input a time in minutes");
+    return false;
+  }
+  if (inputDistance.value === "") {
+    alert("Please input a distance in miles");
+    return false;
+  }
+
+  const json = {
       route: inputRoute.value,
       time: inputTime.value,
-      distance: inputDistance.value,
-      fitness: 0,
+      distance: inputDistance.value
     },
     body = JSON.stringify(json);
 
   fetch("/submit", {
     method: "POST",
-    body,
-  }).then(function (response) {
-    // do something with the reponse
-    loadTable(response);
-  });
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    },
+    body
+  })
+    .then(response => response.json())
+    .then(jsonData => loadTable(jsonData));
 
   return false;
 };
 
-window.onload = function () {
+window.onload = function() {
+  fetch("/loadData", {
+    method: "POST",
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => response.json())
+    .then(jsonData => loadTable(jsonData));
   const button = document.querySelector("button");
   button.onclick = submit;
 };
--- server.improved.js
+++ server.improved.js
@@ -7,16 +7,22 @@ const http = require("http"),
   port = 3000;
 
 const appdata = [
-  { route: "test", time: 7, distance: 1, fitness: 7 },
-  { route: "test", time: 14, distance: 2, fitness: 14 },
-  { route: "test", time: 21, distance: 3, fitness: 21 },
+  { route: "test1", time: 7, distance: 1, fitness: 7 },
+  { route: "test2", time: 14, distance: 2, fitness: 14 },
+  { route: "test3", time: 21, distance: 3, fitness: 21 }
 ];
 
-const calculateFitness = function (record) {
-  record.fitness = record.time*record.distance;
+const sendAppData = function(response) {
+  response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+  response.end(JSON.stringify(appdata));
 };
 
-const server = http.createServer(function (request, response) {
+const calculateFitness = function(record) {
+  return record.time * record.distance;
+};
+
+const server = http.createServer(function(request, response) {
+  console.log(request.method);
   if (request.method === "GET") {
     handleGet(request, response);
   } else if (request.method === "POST") {
@@ -24,7 +30,7 @@ const server = http.createServer(function (request, response) {
   }
 });
 
-const handleGet = function (request, response) {
+const handleGet = function(request, response) {
   const filename = dir + request.url.slice(1);
 
   if (request.url === "/") {
@@ -34,39 +40,37 @@ const handleGet = function (request, response) {
   }
 };
 
-const handlePost = function (request, response) {
-  if (request.url === "/submit") {
-    //if not loading just table, process new record
-    let dataAsString = "";
-    request.on("data", function (dataBuffer) {
-      dataAsString += dataBuffer;
-    });
-    //parse data buffer as string
-
-    request.on("end", function () {
-      var newRecord = JSON.parse(dataAsString); //get new record json
-      calculateFitness(newRecord); //calculate fitness on newRecord (field already present just null)
-
-      appdata.push(newRecord); //put record data into "database"
-    });
+const handlePost = function(request, response) {
+  if (request.url === "/loadData") {
+    sendAppData(response);
+    return true;
   }
-  /*
-  always send table after post
-  any non "/submit" POST request just loads the table
-  ex. "/load"
-  */
-  sendTable(response);
+  let dataAsString = "";
+  request.on("data", function(dataBuffer) {
+    dataAsString += dataBuffer;
+  });
+  //parse data buffer as string
+  request.on("end", function() {
+    if (request.url === "/submit") {
+      var newRecord = JSON.parse(dataAsString); //get new record json
+      console.log(newRecord);
+      appdata.push({
+        route: newRecord.route,
+        time: newRecord.time,
+        distance: newRecord.distance,
+        fitness: calculateFitness(newRecord)
+      }); //put record data into "database"
+    }
+    sendAppData(response);
+  });
+  console.log("appdata\n" + JSON.stringify(appdata));
+  return true;
 };
 
-const sendTable = function (response) {
-  response.writeHead(200, "OK", { "Content-Type": "text/plain" });
-  response.end(JSON.stringify(appdata));
-};
-
-const sendFile = function (response, filename) {
+const sendFile = function(response, filename) {
   const type = mime.getType(filename);
 
-  fs.readFile(filename, function (err, content) {
+  fs.readFile(filename, function(err, content) {
     // if the error = null, then we've loaded the file successfully
     if (err === null) {
       // status code: https://httpstatuses.com
