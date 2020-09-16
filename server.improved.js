30337218c0929dcaf0d93740692ceb8d7da9617d
Danya Baron <danyabaron@Danyas-MacBook-Air.local>
2020-09-15 21:58:09 -0400

    first commit

--- server.improved.js
+++ server.improved.js
@@ -3,52 +3,113 @@ const http = require( 'http' ),
       // IMPORTANT: you must run `npm install` in the directory for this assignment
       // to install the mime library used in the following line of code
       mime = require( 'mime' ),
+      bodyParser = require('body-parser'),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
-  }
-})
+const express = require('express')
+var app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
 
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
+var appdata = [
+  {
+    "Name" : "go grocery shopping",
+    "Priority": "medium",
+    "Deadline": "Thursday"
+  },
+  {
+    "Name": "finish math homework",
+    "Priority": "high",
+    "Deadline": "Wednesday"
+  }]
 
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
-  }
-}
+  app.get('/', function(req, res) {
+    res.sendFile('public/index.html', {root: '.'})
+  })
+  app.get('/scripts.js', function(req, res) {
+    res.sendFile('public/js/scripts.js', {root: '.'})
+  })
+  app.get('/style.css', function(req, res) {
+    res.sendFile('public/css/style.css', {root: '.'})
+  })
 
-const handlePost = function( request, response ) {
+
+app.post('/submit', function (req, res) {
+  console.log(req)
+  console.log(req.body)
   let dataString = ''
 
-  request.on( 'data', function( data ) {
-      dataString += data 
+  res.on( 'data', function( data ) {
+      console.log(data)
+      dataString += data
   })
 
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+  console.log(dataString)
 
-    // ... do something with the data here!!!
+  res.on( 'end',  function() {
+    console.log("test")
+    var jsonData = JSON.parse( dataString )
 
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
+    console.log("test again")
+    let action = jsonData.action
+    console.log("test again2")
+    if(action.includes('add')) {
+      let name = jsonData.taskname
+      let prior = jsonData.taskpriority
+      let deadline = jsonData.taskdeadline
+      let entries = {}
+      entries["Name"] = name
+      entries["Priority"] = prior
+      entries["Deadline"] = deadline
+      console.log(entries)
+      appdata.insert(entries)
+    }
+
+    console.log("test3")
+    if(action.includes('edit')) {
+      let name = jsonData.taskname
+      let prior = jsonData.taskpriority
+      let deadline = jsonData.taskdeadline
+      let newEntries = {}
+      entries["Name"] = name
+      entries["Priority"] = prior
+      entries["Deadline"] = deadline
+      console.log(newEntries)
+      appdata(entries).insert(newEntries)
+    }
+
+    console.log("test3")
+    if(action.includes('delete')) {
+      let name = jsonData.taskname
+      let prior = jsonData.taskpriority
+      let deadline = jsonData.taskdeadline
+      let entries = {}
+      entries["Name"] = name
+      entries["Priority"] = prior
+      entries["Deadline"] = deadline
+      console.log(entries)
+      appdata(entries).remove();
+    }
+
+    console.log("test4")
+    var sendingJSON = {}
+    for(let newEntry in appdata) {
+      sendingJSON.push(newEntry)
+    }
+
+    console.log(sendingJSON)
+
+    res.status(200)
+    res.json(JSON.stringify({}))
+
+})
+
+})
 
 const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+   const type = mime.getType( filename )
 
    fs.readFile( filename, function( err, content ) {
 
@@ -56,17 +117,19 @@ const sendFile = function( response, filename ) {
      if( err === null ) {
 
        // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+       res.writeHeader( 200, { 'Content-Type': type })
+       res.end( content )
 
      }else{
 
        // file not found, error code 404
-       response.writeHeader( 404 )
+       res.writeHeader( 404 )
        response.end( '404 Error: File Not Found' )
 
      }
    })
 }
 
-server.listen( process.env.PORT || port )
+app.listen( port, () => {
+  console.log("starting server")
+})
