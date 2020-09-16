877d4a5a35d911dd43c12a6e4c14399f37ac4bb2
Glitch (a2-alexa-freglette) <none>
2020-09-16 15:18:23 +0000

    🚄🚠 Updated with Glitch

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,15 +1,57 @@
-
-function buildTable(data){
-    var table = document.getElementById('myTable')
-    for (var i = 0; i < data.length; i++){
-    var row = `<tr>
-    <td>${data[i].foodname}</td>
-    <td>${data[i].datebought}</td>
-    <td>${data[i].enjoy}</td>
-    <td>${data[i].size}</td>
-    <td>${data[i].placement}</td>
-    <td>${data[i].dateuseby}</td>
-    </tr>`
-    table.innerHTML += row
+function build (response){
+  var keys = [];
+  
+  var table = document.getElementById("myTable"); 
+  for (var i = 0; i < response.length; i++) {
+  table.deleteRow(-1);
+  }
+for (var i = 0; i < response.length; i++) {
+  var tr = table.insertRow(-1);
+	for (keys in response[i]) {
+    var tabCell = tr.insertCell(-1);
+    tabCell.innerHTML = response[i][keys]
     }
   }
+}
+
+const submit = function( e ) {
+    // prevent default form action from being carried out
+e.preventDefault()
+var food_name = document.querySelector( '#foodname' )
+var datebought = document.querySelector("#purchased")
+var enjoy =document.querySelector("#enjoyability")
+var size = document.querySelector("#size")
+  
+if(food_name.value === ""){
+  alert("Did not fill out food name field.")
+}
+else if(datebought.value === ""){
+  alert("Did not fill out purchased field.")
+}
+else if(enjoy.value === ""){
+  alert("Did not fill out enjoy field.")
+}
+  
+else{ 
+  const
+  json = { foodname: food_name.value ,
+          purchased: datebought.value,
+          enjoyability: enjoy.value,
+          size:size.value},          
+          body = JSON.stringify(json)
+
+
+  fetch( '/submit', {
+  method:'POST',
+  body
+  })
+.then(response => response.json())
+.then(json => build(json))
+return false;
+ }
+}
+
+window.onload = function() {
+const button = document.querySelector( '#btn' )
+button.onclick = submit
+}
--- server.improved.js
+++ server.improved.js
@@ -1,134 +1,77 @@
-const http = require( 'http' ),
-      fs   = require( 'fs' ),
-       IMPORTANT: you must run `npm install` in the directory for this assignment
-       to install the mime library used in the following line of code
-      mime = require( 'mime' ),
-      dir  = 'public/',
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  // IMPORTANT: you must run `npm install` in the directory for this assignment
+  // to install the mime library used in the following line of code
+  mime = require("mime"),
+  dir = "public/",
+  port = 3000;
 
 var myArray = [
-        { 'foodname': 'broccoli', 'datebought': "September 9, 2020", 'enjoy' : 5, 'size': 'small', 'placement': 'top shelf'},
-        { 'foodname': 'spinach', 'datebought': "September 4, 2020",   'enjoy' : 2, 'size': 'large', 'placement': 'middle shelf'},
-        { 'foodname': 'cookies', 'datebought': "September 1, 2020",  'enjoy' : 5, 'size': 'medium', 'placement': 'bottom shelf'},
-        { 'foodname': 'honey', 'datebought': "September 1, 2020",  'enjoy' : 5, 'size': 'small', 'placement': 'middle shelf'},
+        { 'foodname': 'broccoli', 'datebought': "2020-09-12", 'enjoy' : 5, 'size': 'small', 'placement': 'top shelf'},
+        { 'foodname': 'spinach', 'datebought': "2020-09-3",   'enjoy' : 2, 'size': 'large', 'placement': 'middle shelf'},
+        { 'foodname': 'cookies', 'datebought': "2020-09-01",  'enjoy' : 5, 'size': 'medium', 'placement': 'bottom shelf'},
+        { 'foodname': 'honey', 'datebought': "2020-09-11",  'enjoy' : 5, 'size': 'small', 'placement': 'middle shelf'},
 ]
 
 
-const server = http.createServer( function( request,response ) {
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
+const server = http.createServer(function(request, response) {
+  if (request.method === "GET") {
+    handleGet(request, response);
+  } else if (request.method === "POST") {
+    handlePost(request, response);
   }
-})
-
-const handleGet = function( request, response ) {
-  const filename = dir + request.url.slice( 1 ) 
-
-  if( request.url === '/' ) {
-    sendFile( response, 'public/index.html' )
-  }else{
-    sendFile( response, filename )
-  }
-}
-
-const handlePost = function( request, response ) {
-  let dataString = ''
-
-  request.on( 'data', function( data ) {
-      dataString += data 
-  })
-
-  request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
-
-    const handleFormSubmit = (event) => {
-      // Stop the form from submitting since we’re handling that with AJAX.
-      event.preventDefault();
-      // TODO: Call our function to get the form data.
-      var x = 4
-      var foodname = document.getElementById("foodname").value
-      var datebought =document.getElementById("purchased").value
-      var enjoy =document.getElementById("enjoyability").value
-      var size = document.getElementById("size").value
-      function placement(){
-      if(size === "small" || enjoy >3){
-        return "top shelf"
-        }
-        if(size === "medium" && enjoy === 2){
-        return "middle shelf"
-        }
-        else{
-          return "bottom shelf"
-        }
-      }
-      
-      const data = {
-          foodname,
-          datebought,
-          enjoy,
-          size,
-          placement: placement(),
-          dateuseby: "bruh"
-      }
-      myArray.push(data);
-      console.warn('added', {myArray});
-     let pre = document.querySelector('#msg pre');
-      var table = document.getElementById('myTable');
-      if((document.getElementById("size").value === "small") && ((document.getElementById("enjoyability").value)>3))
-      {
-        myArray[i].placement = "top shelf";
-      }
-      
-          for (var i = myArray.length-1; i < myArray.length; i++){
-           //var checkoutDate = new Date();    // Thu Jul 21 2016 10:05:13 GMT-0400 (EDT)
-            //checkoutDate.setDate(myArray[i].datebought.getDate() + 5 );
-             
-            var row = `<tr>
-                              <td>${myArray[i].foodname}</td>
-                              <td>${myArray[i].datebought}</td>
-                              <td>${myArray[i].enjoy}</td>
-                              <td>${myArray[i].size}</td>
-                              <td>${myArray[i].placement}</td>
-                              <td>${myArray[i].datetouse}</td>
-                        </tr>`
-              table.innerHTML += row
-          }
-  
-  
-  }
-
-  document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('btn').addEventListener('click', handleFormSubmit);
-    document.getElementById('btn').addEventListener('click', buildTable(myArray));
-
-
 });
-    response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end()
-  })
-}
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+const handleGet = function(request, response) {
+  const filename = dir + request.url.slice(1);
 
-   fs.readFile( filename, function( err, content ) {
+  if (request.url === "/") {
+    sendFile(response, "public/index.html");
+  } else {
+    sendFile(response, filename);
+  }
+};
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+const handlePost = function(request, response) {
+  let dataString = "";
+  request.on("data", function(data) {
+    dataString += data;
+  });
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+  request.on("end", function() {
+  var parsed = JSON.parse(dataString); 
+    
+  if(parsed.size === "small" || parsed.enjoy >3){
+   parsed.placement = "top shelf"
+  }else if (parsed.size === "medium" || parsed.enjoy === 2){
+    parsed.placement = "medium shelf"}
+  else{
+      parsed.placement = "bottom shelf"
+ }
+    myArray.push(parsed)
+    response.writeHead(200, "OK", { "Content-Type": "text/plain" });
+    console.log(myArray)
+    response.end(JSON.stringify(myArray));
 
-     }else{
+  });
+};
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+const sendFile = function(response, filename) {
+  const type = mime.getType(filename);
 
-     }
-   })
-}
+  fs.readFile(filename, function(err, content) {
+    // if the error = null, then we've loaded the file successfully
+    if (err === null) {
+      // status code: https://httpstatuses.com
+      response.writeHeader(200, { "Content-Type": type });
+      response.end(content);
+    } else {
+      // file not found, error code 404
+      response.writeHeader(404);
+      response.end("404 Error: File Not Found");
+    }
+  });
+};
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
+
