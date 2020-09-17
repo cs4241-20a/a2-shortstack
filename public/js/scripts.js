63a1bfec6f3243f8e400958628721f06509e7636
Benny Klaiman <47163915+bnklaiman@users.noreply.github.com>
2020-09-16 23:18:30 -0400

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,34 @@
-// Add some Javascript code here, to run on the front end.
+const submit = function(e) {
+  // prevent default form action from being carried out
+  e.preventDefault();
 
-console.log("Welcome to assignment 2!")
+  const taskName = document.querySelector("#taskName");
+  const taskPriority = document.querySelector("#taskPriority");
+  const taskComplete = document.querySelector("#taskComplete");
+  const currentDate = new Date();
+  const taskDueDate = new Date(currentDate);
+  taskDueDate.setDate(taskDueDate.getDate() + 1);
+
+  let json = {
+    task: taskName.value,
+    priority: parseInt(taskPriority.value),
+    isTaskComplete: taskComplete.checked,
+    taskDue: taskDueDate.value
+  };
+
+  const body = JSON.stringify(json);
+
+  fetch("/submit", {
+    method: "POST",
+    body
+  }).then(function(response) {
+    // do something with the reponse
+  });
+
+  return false;
+};
+
+window.onload = function() {
+  const button = document.querySelector("button");
+  button.onclick = submit;
+};
