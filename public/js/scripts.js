9cd6613ef0f1df0cc7841098d0069d1255d92789
Shadorian <dogleycat@gmail.com>
2020-09-16 23:51:37 -0400

    all the things

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,7 @@
-// Add some Javascript code here, to run on the front end.
+const ACTION_NONE = "none"
+const ACTION_EDIT = "edit"
+const ACTION_NEW = "new"
+const ACTION_DELETE = "delete"
 
 console.log("Welcome to assignment 2!")
 
@@ -6,10 +9,11 @@ const submit = function( e ) {
     // prevent default form action from being carried out
     e.preventDefault()
 
-    const input = document.querySelector( '#yourname' ),
-          json = { yourname: input.value },
-          body = JSON.stringify( json )
-          console.log( "client: " + body )
+    const inputName = document.querySelector( '#nameInput' )
+    const inputScore = document.querySelector( '#scoreInput' )
+    const inputPossible = document.querySelector( '#possibleInput' )
+    const json = { "inputName": inputName.value, "inputScore": inputScore.value, "inputPossible": inputPossible.value }
+    const body = JSON.stringify( json )
 
     fetch( '/submit', {
       method:'POST',
@@ -17,10 +21,24 @@ const submit = function( e ) {
     })
     .then( function( response ) {
         //console.log(response)
-        return response.text()
+        return response.json()
     })
     .then( function( json ) {
-        console.log( json )
+      console.log( "Total: " + json.total)
+      
+      const table = document.querySelector("#data_display");
+      const messagebox = document.querySelector("#message_box")
+      const totalbox = document.querySelector("#total_box")
+      
+      messagebox.innerHTML = json.message
+      totalbox.innerHTML = "Overall score: " + json.total + "%"
+      
+      for (let i = table.rows.length-1; i > 0 ; i--) {
+        table.deleteRow(i)
+      }
+      for (let i = 0; i < json.grades.length; i++) {  
+        addtoTable( table, json.grades[i] )
+      }
     })
 
     return false
@@ -29,4 +47,16 @@ const submit = function( e ) {
   window.onload = function() {
     const button = document.querySelector( '#submitter' )
     button.onclick = submit
+  }
+
+  function addtoTable( table, assignment ) {
+    const row = table.insertRow(table.length)
+    const c1 = row.insertCell(0)
+    const c2 = row.insertCell(1)
+    const c3 = row.insertCell(2)
+    const c4 = row.insertCell(3)
+    c1.innerHTML = assignment.assignName
+    c2.innerHTML = assignment.assignScore
+    c3.innerHTML = assignment.assignPossible
+    c4.innerHTML = assignment.percent
   }
--- server.improved.js
+++ server.improved.js
@@ -6,11 +6,17 @@ const http = require( 'http' ),
       dir  = 'public/',
       port = 3000
 
-const appdata = [
-  { 'model': 'toyota', 'year': 1999, 'mpg': 23 },
-  { 'model': 'honda', 'year': 2004, 'mpg': 30 },
-  { 'model': 'ford', 'year': 1987, 'mpg': 14} 
-]
+const MSG_ERR_NAME_BLANK = "Error: the Name field must not be empty."
+const MSG_ERR_FIELD_MISMATCH = "Error: invalid input.  Score & Value fields must either be both empty or both filled."
+const MSG_ERR_NON_NUMERIC = "Error: the Score and Value fields must only contain numbers."
+const MSG_ERR_NO_ASSIGNMENT = "Error: no assignment with this name exists.  To create one, fill in the Score and Value fields."
+const MSG_SUC_ASSIGN_ADD = "Successfully added a new assignment."
+const MSG_SUC_ASSIGN_EDIT = "Successfully edited assignment."
+const MSG_SUC_ASSIGN_REMOVE = "Successfully removed assignment."
+const ACTION_NONE = "none"
+const ACTION_EDIT = "edit"
+const ACTION_NEW = "new"
+const ACTION_DELETE = "delete"
 
 const server = http.createServer( function( request,response ) {
   if( request.method === 'GET' ) {
@@ -30,19 +36,101 @@ const handleGet = function( request, response ) {
   }
 }
 
+let grades = []
+function getTotalPercent() {
+  let pointsAchieved = 0
+  let pointsPossible = 0
+  
+  for(let i = 0; i<grades.length; i++) {
+    pointsAchieved += grades[i].assignScore
+    pointsPossible += grades[i].assignPossible
+  }
+  
+  return Math.round(pointsAchieved / pointsPossible * 10000 ) / 100
+}
+
+function getAssignPercent(score, possible) {
+  return Math.round( score / possible * 10000 ) / 100
+}
+
+
+function handleInput(reply, input) {
+  
+  const assignName = input.inputName
+  if(assignName === "") {
+    reply.message = MSG_ERR_NAME_BLANK
+    reply.action = ACTION_NONE
+    return
+  }
+  if( (input.inputScore === "") !== (input.inputPossible === "") ) {
+    // ensure score & possible are either both full or both blank
+    reply.message = MSG_ERR_FIELD_MISMATCH
+    reply.action = ACTION_NONE
+    return
+  }
+  const assignScore = Number(input.inputScore)
+  const assignPossible = Number(input.inputPossible)
+  if( Number.isNaN(assignScore) || Number.isNaN(assignPossible) ) {
+    // ensure numeric fields are actually numeric
+    reply.message = MSG_ERR_NON_NUMERIC
+    reply.action = ACTION_NONE
+    return
+  }
+  
+  for (let i = 0; i<grades.length; i++) {
+    if( grades[i].assignName === assignName ) {
+      if( input.inputScore === "") {
+        // delete item
+        grades.splice(i)
+        reply.message = MSG_SUC_ASSIGN_REMOVE
+        reply.action = ACTION_DELETE
+        return
+        
+      } else {
+        // edit item  
+        grades[i].assignScore = assignScore
+        grades[i].assignPossible = assignPossible
+        grades[i].percent = getAssignPercent( assignScore, assignPossible )
+        reply.message = MSG_SUC_ASSIGN_EDIT
+        reply.action = ACTION_EDIT
+        return
+      }
+    }
+  }
+  // if no match, create new assign
+  if( input.inputScore === "") {
+    // but first check that fields are filled in
+    reply.message = MSG_ERR_NO_ASSIGNMENT
+    reply.action = ACTION_NONE
+    return
+  }
+  
+  let assignment = { assignName: assignName, assignScore: assignScore, assignPossible: assignPossible }
+  assignment.percent = getAssignPercent( assignScore, assignPossible )
+  grades.push( assignment )
+  
+  reply.message = MSG_SUC_ASSIGN_ADD
+  reply.action = ACTION_NEW
+  return
+}
+
 const handlePost = function( request, response ) {
-  let dataString = "default string"
+  let jsonResponse = {}
 
   request.on( 'data', function( data ) {
-      dataString += JSON.parse( data ) 
+    const inputJson = JSON.parse( data )
+    handleInput( jsonResponse, inputJson )
+    
+    jsonResponse.grades = grades
+    jsonResponse.total = getTotalPercent()
+    
+    
   })
 
   request.on( 'end', function() {
 
-    // ... do something with the data here!!!
-
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' })
-    response.end( dataString )
+    response.end( JSON.stringify(jsonResponse) )
   })
 }
 
